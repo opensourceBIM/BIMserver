a5a587f42e10ab32cb966fd1b434d913de602197
Ruben de Laat <ruben@logic-labs.nl>
2011-03-22 15:18:08 +0000

    http://code.google.com/p/bimserver/issues/detail?id=151&sort=milestone%20priority

--- BimServer/src/org/bimserver/ReflectiveRpcChannel.java
+++ BimServer/src/org/bimserver/ReflectiveRpcChannel.java
@@ -1,18 +1,32 @@
 package org.bimserver;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.activation.DataHandler;
+
+import org.apache.commons.io.IOUtils;
 import org.bimserver.models.log.AccessMethod;
+import org.bimserver.pb.Service.ResultType;
 import org.bimserver.shared.ServiceInterface;
+import org.bimserver.utils.StringUtils;
 import org.bimserver.webservices.ServiceFactory;
 
 import com.google.protobuf.BlockingRpcChannel;
+import com.google.protobuf.ByteString;
 import com.google.protobuf.Message;
 import com.google.protobuf.RpcController;
 import com.google.protobuf.ServiceException;
 import com.google.protobuf.Descriptors.Descriptor;
+import com.google.protobuf.Descriptors.EnumValueDescriptor;
 import com.google.protobuf.Descriptors.FieldDescriptor;
 import com.google.protobuf.Descriptors.MethodDescriptor;
 import com.google.protobuf.Descriptors.FieldDescriptor.JavaType;
@@ -36,14 +50,14 @@ public class ReflectiveRpcChannel implements BlockingRpcChannel {
 		Class[] parameterClasses = new Class[methodDescriptor.getInputType().getFields().size()];
 		int ci=0;
 		for (FieldDescriptor fieldDescriptor : methodDescriptor.getInputType().getFields()) {
-			parameterClasses[ci++] = convert(fieldDescriptor.getJavaType());
+			parameterClasses[ci++] = convert(fieldDescriptor);
 		}
 		try {
 			Method method = clazz.getMethod(methodDescriptor.getName(), parameterClasses);
 			Object[] arguments = new Object[methodDescriptor.getInputType().getFields().size()];
 			int i=0;
 			for (FieldDescriptor fieldDescriptor : methodDescriptor.getInputType().getFields()) {
-				arguments[i] = request.getField(fieldDescriptor);
+				arguments[i] = convertProtocolBuffersObjectToServiceObject(request.getField(fieldDescriptor));
 				i++;
 			}
 			Object result = method.invoke(service, arguments);
@@ -54,14 +68,15 @@ public class ReflectiveRpcChannel implements BlockingRpcChannel {
 				builder.setField(valueField, result);
 			} else if (result instanceof List) {
 				Descriptor messageType = valueField.getMessageType();
-				List list = (List) builder.getField(valueField);
+				List list = new ArrayList();
+				List originalList = (List) result;
+				for (Object object : originalList) {
+					list.add(convertObject(new HashMap(), messageType, object));
+				}
+				builder.setField(valueField, list);
 			} else {
 				Descriptor messageType = valueField.getMessageType();
-				Object field = builder.getField(valueField);
-//			for (FieldDescriptor fieldDescriptor : methodDescriptor.getOutputType().getFields()) {
-//				Method method2 = resultClass.getMethod("get" + StringUtils.firstUpperCase(fieldDescriptor.getName()));
-//				builder.setField(fieldDescriptor, method2.invoke(result));
-//			}
+				builder.setField(valueField, convertObject(new HashMap<Object, Object>(), messageType, result));
 			}
 			return builder.build();
 		} catch (SecurityException e) {
@@ -80,21 +95,114 @@ public class ReflectiveRpcChannel implements BlockingRpcChannel {
 		return null;
 	}
 
-	private Class convert(JavaType javaType) {
-		if (javaType == JavaType.BOOLEAN) {
-			return Boolean.class;
-		} else if (javaType == JavaType.BYTE_STRING) {
+	private Object convertProtocolBuffersObjectToServiceObject(Object value) {
+		if (value instanceof EnumValueDescriptor) {
+			EnumValueDescriptor enumValueDescriptor = (EnumValueDescriptor)value;
+			int ordinal = enumValueDescriptor.getNumber();
+			return org.bimserver.shared.ResultType.values()[ordinal];
+		} else {
+			return value;
+		}
+	}
+
+	private Object convertObject(Map<Object, Object> convertedObjects, Descriptor descriptor, Object object) {
+		if (convertedObjects.containsKey(object)) {
+			return convertedObjects.get(object);
+		}
+		Class<? extends Object> clazz = object.getClass();
+		Builder builder = null;
+		try {
+			Class builderClass = Class.forName("org.bimserver.pb.Service$" + descriptor.getName());
+			Method method = builderClass.getMethod("newBuilder");
+			builder = (Builder) method.invoke(null);
+			convertedObjects.put(object, builder);
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		for (FieldDescriptor fieldDescriptor : descriptor.getFields()) {
+			try {
+				Method getMethod = clazz.getMethod("get" + StringUtils.firstUpperCase(fieldDescriptor.getName()));
+				Object value = getMethod.invoke(object);
+				if (value != null) {
+					if (value.getClass().isPrimitive() || value.getClass() == String.class || value.getClass() == Long.class || value.getClass() == Float.class || value.getClass() == Integer.class) {
+						builder.setField(fieldDescriptor, value);
+					} else if (value.getClass().isEnum()) {
+						Enum eNum = (Enum)value;
+						int ordinal = eNum.ordinal();
+						EnumValueDescriptor findValueByNumber = fieldDescriptor.getEnumType().findValueByNumber(ordinal);
+						builder.setField(fieldDescriptor, findValueByNumber);
+					} else if (value.getClass() == Date.class) {
+						builder.setField(fieldDescriptor, ((Date)value).getTime());
+					} else if (value.getClass() == DataHandler.class) {
+						DataHandler dataHandler = (DataHandler)value;
+						InputStream inputStream = dataHandler.getInputStream();
+						ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+						IOUtils.copy(inputStream, byteArrayOutputStream);
+						ByteString byteString = ByteString.copyFrom(byteArrayOutputStream.toByteArray());
+						builder.setField(fieldDescriptor, byteString);
+					} else if (List.class.isAssignableFrom(value.getClass())) {
+						List list = (List)value;
+						List newList = new ArrayList();
+						for (Object o : list) {
+							if (fieldDescriptor.getJavaType() == JavaType.MESSAGE) {
+								newList.add(convertObject(convertedObjects, fieldDescriptor.getMessageType(), o));
+							} else {
+								newList.add(o);
+							}
+						}
+						builder.setField(fieldDescriptor, newList);
+					} else {
+						System.out.println("Unimplemented: " + fieldDescriptor.getName() + ": " + value);
+					}
+				}
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return builder.build();
+	}
+
+	private Class convert(FieldDescriptor fieldDescriptor) {
+		if (fieldDescriptor.getJavaType() == JavaType.BOOLEAN) {
+			return boolean.class;
+		} else if (fieldDescriptor.getJavaType() == JavaType.BYTE_STRING) {
 			return Byte[].class;
-		} else if (javaType == JavaType.DOUBLE) {
+		} else if (fieldDescriptor.getJavaType() == JavaType.DOUBLE) {
 			return Double.class;
-		} else if (javaType == JavaType.FLOAT) {
+		} else if (fieldDescriptor.getJavaType() == JavaType.FLOAT) {
 			return Float.class;
-		} else if (javaType == JavaType.INT) {
+		} else if (fieldDescriptor.getJavaType() == JavaType.INT) {
 			return Integer.class;
-		} else if (javaType == JavaType.LONG) {
-			return Long.class;
-		} else if (javaType == JavaType.STRING) {
+		} else if (fieldDescriptor.getJavaType() == JavaType.LONG) {
+			return long.class;
+		} else if (fieldDescriptor.getJavaType() == JavaType.STRING) {
 			return String.class;
+		} else if (fieldDescriptor.getJavaType() == JavaType.ENUM) {
+			try {
+				return Class.forName("org.bimserver.shared." + fieldDescriptor.getEnumType().getName());
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
 		}
 		return null;
 	}
--- BimServer/src/org/bimserver/pb/client/ProtocolBuffersClient.java
+++ BimServer/src/org/bimserver/pb/client/ProtocolBuffersClient.java
@@ -1,15 +1,25 @@
 package org.bimserver.pb.client;
 
+import org.bimserver.pb.Service.DownloadRequest;
+import org.bimserver.pb.Service.DownloadResponse;
 import org.bimserver.pb.Service.GetAllProjectsRequest;
 import org.bimserver.pb.Service.GetAllProjectsResponse;
+import org.bimserver.pb.Service.GetDownloadDataRequest;
+import org.bimserver.pb.Service.GetDownloadDataRequestOrBuilder;
+import org.bimserver.pb.Service.GetDownloadDataResponse;
+import org.bimserver.pb.Service.GetRevisionRequest;
+import org.bimserver.pb.Service.GetRevisionResponse;
 import org.bimserver.pb.Service.LoginRequest;
 import org.bimserver.pb.Service.LoginResponse;
+import org.bimserver.pb.Service.ResultType;
+import org.bimserver.pb.Service.SDownloadResult;
 import org.bimserver.pb.Service.SProject;
 import org.bimserver.pb.Service.ServiceInterface;
 import org.bimserver.pb.Service.GetAllProjectsRequest.Builder;
 import org.bimserver.pb.Service.ServiceInterface.BlockingInterface;
 
 import com.google.protobuf.BlockingRpcChannel;
+import com.google.protobuf.ByteString;
 import com.google.protobuf.ServiceException;
 import com.googlecode.protobuf.socketrpc.RpcChannels;
 import com.googlecode.protobuf.socketrpc.SocketRpcConnectionFactories;
@@ -30,7 +40,12 @@ public class ProtocolBuffersClient {
 		service = ServiceInterface.newBlockingStub(rpcChannel);
 		try {
 			login("admin@logic-labs.nl", "admin");
-			getAllProjects();
+			SProject firstProject = getAllProjects();
+			for (long roid : firstProject.getRevisionsList()) {
+				System.out.println(roid);
+				getRevision(roid);
+				String downloadId = downloadRevision(roid);
+			}
 		} catch (ServiceException e) {
 			e.printStackTrace();
 		}
@@ -41,16 +56,58 @@ public class ProtocolBuffersClient {
 		}
 	}
 
-	private void getAllProjects() throws ServiceException {
+	private String downloadRevision(long roid) {
+		org.bimserver.pb.Service.DownloadRequest.Builder downloadRequestBuilder = DownloadRequest.newBuilder();
+		downloadRequestBuilder.setRoid(roid);
+		downloadRequestBuilder.setResultType(ResultType.O3D_JSON);
+		downloadRequestBuilder.setSync(true);
+		DownloadRequest downloadRequest = downloadRequestBuilder.build();
+		try {
+			DownloadResponse downloadResponse = service.download(rpcController, downloadRequest);
+			if (downloadResponse != null) {
+				String downloadId = downloadResponse.getValue();
+				
+				org.bimserver.pb.Service.GetDownloadDataRequest.Builder getDownloadDataRequestBuilder = GetDownloadDataRequest.newBuilder();
+				getDownloadDataRequestBuilder.setActionID(downloadId);
+				GetDownloadDataRequest getDownloadDataRequest = getDownloadDataRequestBuilder.build();
+				
+				GetDownloadDataResponse getDownloadDataResponse = service.getDownloadData(rpcController, getDownloadDataRequest);
+				if (getDownloadDataResponse != null) {
+					SDownloadResult sDownloadResult = getDownloadDataResponse.getValue();
+					ByteString file = sDownloadResult.getFile();
+					System.out.println(file.size());
+				}
+			}
+		} catch (ServiceException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private void getRevision(long roid) throws ServiceException {
+		org.bimserver.pb.Service.GetRevisionRequest.Builder getRevisionRequestBuilder = GetRevisionRequest.newBuilder();
+		getRevisionRequestBuilder.setRoid(roid);
+		GetRevisionRequest getRevisionRequest = getRevisionRequestBuilder.build();
+		
+		GetRevisionResponse getRevisionResponse = service.getRevision(rpcController, getRevisionRequest);
+		System.out.println(getRevisionResponse.getValue().getSize());
+	}
+
+	private SProject getAllProjects() throws ServiceException {
 		Builder getAllProjectsRequestBuilder = GetAllProjectsRequest.newBuilder();
 		GetAllProjectsRequest getAllProjectsRequest = getAllProjectsRequestBuilder.build();
 		
 		GetAllProjectsResponse getAllProjectsResponse = service.getAllProjects(rpcController, getAllProjectsRequest);
+		SProject firstProject = null;
 		if (getAllProjectsResponse != null) {
 			for (SProject sProject : getAllProjectsResponse.getValueList()) {
+				if (firstProject == null) {
+					firstProject = sProject;
+				}
 				System.out.println(sProject.getName());
 			}
 		}
+		return firstProject;
 	}
 
 	private void login(String username, String password) throws ServiceException {
