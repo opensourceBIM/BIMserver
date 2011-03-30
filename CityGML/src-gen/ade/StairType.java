9193036aac7857118a48716a2330ab7878ee6736
Ruben de Laat <ruben@logic-labs.nl>
2011-03-30 08:48:33 +0000

    CityGML might be broken after this, will fix asap

--- CityGML/src-gen/ade/StairType.java
+++ CityGML/src-gen/ade/StairType.java
@@ -3,7 +3,6 @@ package ade;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
---
+++ CityGML/src/org/bimserver/citygml/Test.java
@@ -0,0 +1,51 @@
+package org.bimserver.citygml;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import nl.tue.buildingsmart.express.dictionary.SchemaDefinition;
+
+import org.bimserver.ifc.FileFieldIgnoreMap;
+import org.bimserver.ifc.IfcModel;
+import org.bimserver.ifc.SchemaLoader;
+import org.bimserver.ifc.SerializerException;
+import org.bimserver.ifc.file.reader.IfcStepDeserializer;
+import org.bimserver.ifc.file.reader.IncorrectIfcFileException;
+import org.bimserver.ifcengine.IfcEngineFactory;
+import org.bimserver.models.ifc2x3.Ifc2x3Package;
+import org.bimserver.shared.LocalDevelopmentResourceFetcher;
+import org.bimserver.shared.ResourceFetcher;
+import org.bimserver.tests.TestFile;
+import org.bimserver.utils.CollectionUtils;
+
+public class Test {
+	public static void main(String[] args) {
+		SchemaDefinition schema = SchemaLoader.loadDefaultSchema();
+		IfcStepDeserializer fastIfcFileReader = new IfcStepDeserializer(schema);
+		ResourceFetcher resourceFetcher = new LocalDevelopmentResourceFetcher();
+		FileFieldIgnoreMap fieldIgnoreMap = new FileFieldIgnoreMap(CollectionUtils.singleSet(Ifc2x3Package.eINSTANCE), resourceFetcher);
+		File nativeFolder = resourceFetcher.getFile("lib/" + File.separator + System.getProperty("sun.arch.data.model"));
+		IfcEngineFactory ifcEngineFactory = new IfcEngineFactory(SchemaLoader.DEFAULT_SCHEMA_FILE, nativeFolder, new File("tmp"), null);
+		try {
+			IfcModel model = fastIfcFileReader.read(TestFile.AC11.getFile());
+			try {
+				CityGmlSerializer cityGmlSerializer = new CityGmlSerializer(null, null, "test", model, schema, fieldIgnoreMap, ifcEngineFactory);
+				FileOutputStream fos = new FileOutputStream(new File("out.citygml"));
+				cityGmlSerializer.write(fos);
+				fos.close();
+			} catch (SerializerException e) {
+				e.printStackTrace();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (IncorrectIfcFileException e1) {
+			e1.printStackTrace();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+	}
+}
