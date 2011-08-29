0e7c2f2d03af2dd694836cde3b111fecbbe2aed1
Ruben de Laat <ruben@logic-labs.nl>
2011-08-29 15:07:20 +0000

    - Added the BimServerClientLib, client-side EMF

---
+++ BimServerClientLib/src/org/bimserver/client/BimServerClient.java
@@ -0,0 +1,51 @@
+package org.bimserver.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.ws.BindingProvider;
+
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.bimserver.shared.ServiceInterface;
+
+public class BimServerClient {
+	private ServiceInterface serviceInterface;
+
+
+	public BimServerClient(String address) {
+		serviceInterface = createClient(address);
+	}
+	
+	public BimServerClient(ServiceInterface serviceInterface) {
+		this.serviceInterface = serviceInterface;
+	}
+
+	private ServiceInterface createClient(final String address) {
+		JaxWsProxyFactoryBean cpfb = new JaxWsProxyFactoryBean();
+		cpfb.setServiceClass(ServiceInterface.class);
+		cpfb.setAddress(address);
+		Map<String, Object> properties = new HashMap<String, Object>();
+		properties.put("mtom-enabled", Boolean.TRUE);
+		cpfb.setProperties(properties);
+
+		ServiceInterface service = (ServiceInterface) cpfb.create();
+
+		Client client = ClientProxy.getClient(service);
+		HTTPConduit http = (HTTPConduit) client.getConduit();
+		((BindingProvider) service).getRequestContext().put(BindingProvider.SESSION_MAINTAIN_PROPERTY, Boolean.TRUE);
+		http.getClient().setConnectionTimeout(360000);
+		http.getClient().setAllowChunking(false);
+		http.getClient().setReceiveTimeout(320000);
+		
+		return service;
+	}
+
+	
+	public Session createSession() {
+		Session session = new Session(serviceInterface);
+		return session;
+	}
+}
---
+++ BimServerClientLib/src/org/bimserver/client/Session.java
@@ -0,0 +1,93 @@
+package org.bimserver.client;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.bimserver.emf.IdEObject;
+import org.bimserver.models.ifc2x3.Ifc2x3Factory;
+import org.bimserver.models.ifc2x3.Ifc2x3Package;
+import org.bimserver.shared.ServiceInterface;
+import org.bimserver.shared.UserException;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.EcorePackage;
+
+public class Session {
+
+	private ServiceInterface serviceInterface;
+	private Set<IdEObject> newObjects = new HashSet<IdEObject>();
+
+	public Session(ServiceInterface serviceInterface) {
+		this.serviceInterface = serviceInterface;
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T extends IdEObject> T create(Class<T> cl) {
+		EClass eClass = (EClass) Ifc2x3Package.eINSTANCE.getEClassifier(cl.getSimpleName());
+		IdEObject eObject = (IdEObject) Ifc2x3Factory.eINSTANCE.create(eClass);
+		try {
+			Long oid = serviceInterface.createObject(cl.getSimpleName());
+			newObjects.add(eObject);
+			eObject.setOid(oid);
+		} catch (UserException e) {
+			e.printStackTrace();
+		}
+		return (T) eObject;
+	}
+
+	public void startTransaction(int pid) {
+		try {
+			serviceInterface.startTransaction(pid);
+		} catch (UserException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public long commitTransaction() {
+		try {
+			for (IdEObject eObject : newObjects) {
+				for (EStructuralFeature eStructuralFeature : eObject.eClass().getEAllStructuralFeatures()) {
+					Object val = eObject.eGet(eStructuralFeature);
+					if (eStructuralFeature instanceof EAttribute) {
+						EAttribute eAttribute = (EAttribute) eStructuralFeature;
+						if (eAttribute.isMany()) {
+							if (eAttribute.getEType() == EcorePackage.eINSTANCE.getEString()) {
+								serviceInterface.addStringAttribute(eObject.getOid(), eObject.eClass().getName(), eAttribute.getName(), (String) val);
+							} else if (eAttribute.getEType() == EcorePackage.eINSTANCE.getEInt()) {
+								serviceInterface.addIntegerAttribute(eObject.getOid(), eObject.eClass().getName(), eAttribute.getName(), (Integer) val);
+							} else if (eAttribute.getEType() == EcorePackage.eINSTANCE.getEFloat()) {
+								serviceInterface.addFloatAttribute(eObject.getOid(), eObject.eClass().getName(), eAttribute.getName(), (Float) val);
+							} else {
+								throw new RuntimeException("Unimplemented: " + eAttribute.getEType());
+							}
+						} else {
+							if (eAttribute.getEType() == EcorePackage.eINSTANCE.getEString()) {
+								serviceInterface.setStringAttribute(eObject.getOid(), eObject.eClass().getName(), eAttribute.getName(), (String) val);
+							} else if (eAttribute.getEType() == EcorePackage.eINSTANCE.getEInt()) {
+								serviceInterface.setIntegerAttribute(eObject.getOid(), eObject.eClass().getName(), eAttribute.getName(), (Integer) val);
+							} else if (eAttribute.getEType() == EcorePackage.eINSTANCE.getEFloat()) {
+								serviceInterface.setFloatAttribute(eObject.getOid(), eObject.eClass().getName(), eAttribute.getName(), (Float) val);
+							} else {
+								throw new RuntimeException("Unimplemented: " + eAttribute.getEType());
+							}
+						}
+					} else if (eStructuralFeature instanceof EReference) {
+						EReference eReference = (EReference)eStructuralFeature;
+						IdEObject referredObject = (IdEObject)val;
+						if (eReference.isMany()) {
+							serviceInterface.addReference(eObject.getOid(), eObject.eClass().getName(), eReference.getName(), referredObject.getOid(), referredObject.eClass().getName());
+						} else {
+							serviceInterface.setReference(eObject.getOid(), eObject.eClass().getName(), eReference.getName(), referredObject.getOid(), referredObject.eClass().getName());
+						}
+					}
+				}
+			}
+			return serviceInterface.commitTransaction();
+		} catch (UserException e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
+}
