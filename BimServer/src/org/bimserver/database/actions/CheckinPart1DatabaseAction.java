b7e2057ba015cbf896437033e1a610e2a8ac7202
Ruben de Laat <ruben@logic-labs.nl>
2011-08-30 13:54:06 +0000

    Fixed bug when storing the same object multiple times

--- BimServer/src/org/bimserver/database/actions/CheckinPart1DatabaseAction.java
+++ BimServer/src/org/bimserver/database/actions/CheckinPart1DatabaseAction.java
@@ -33,34 +33,38 @@ public class CheckinPart1DatabaseAction extends GenericCheckinDatabaseAction {
 
 	@Override
 	public ConcreteRevision execute() throws UserException, BimDeadlockException, BimDatabaseException {
-		Project project = getProjectByPoid(poid);
-		User user = getUserByUoid(actingUid);
-		if (project == null) {
-			throw new UserException("Project with poid " + poid + " not found");
+		try {
+			Project project = getProjectByPoid(poid);
+			User user = getUserByUoid(actingUid);
+			if (project == null) {
+				throw new UserException("Project with poid " + poid + " not found");
+			}
+			if (user.getUserType() == UserType.ANONYMOUS) {
+				throw new UserException("User anonymous cannot create new revisions");
+			}
+			if (!RightsManager.hasRightsOnProjectOrSuperProjects(user, project)) {
+				throw new UserException("User has no rights to checkin models to this project");
+			}
+			if (!MailSystem.isValidEmailAddress(user.getUsername())) {
+				throw new UserException("Users must have a valid e-mail address to checkin");
+			}
+			checkCheckSum(project);
+			if (!project.getRevisions().isEmpty() && project.getRevisions().get(project.getRevisions().size() - 1).getState() == CheckinState.STORING) {
+				throw new UserException("Another checkin on this project is currently running, please wait and try again");
+			}
+			ConcreteRevision concreteRevision = createNewConcreteRevision(getDatabaseSession(), getModel().getSize(), poid, actingUid, comment.trim(), CheckinState.STORING);
+			concreteRevision.setChecksum(getModel().getChecksum());
+			NewRevisionAdded newRevisionAdded = LogFactory.eINSTANCE.createNewRevisionAdded();
+			newRevisionAdded.setDate(new Date());
+			newRevisionAdded.setExecutor(user);
+			newRevisionAdded.setRevision(concreteRevision.getRevisions().get(0));
+			newRevisionAdded.setAccessMethod(getAccessMethod());
+			getDatabaseSession().store(newRevisionAdded);
+			getDatabaseSession().store(concreteRevision);
+			getDatabaseSession().store(project);
+			return concreteRevision;
+		} catch (Exception e) {
+			throw new UserException(e);
 		}
-		if (user.getUserType() == UserType.ANONYMOUS) {
-			throw new UserException("User anonymous cannot create new revisions");
-		}
-		if (!RightsManager.hasRightsOnProjectOrSuperProjects(user, project)) {
-			throw new UserException("User has no rights to checkin models to this project");
-		}
-		if (!MailSystem.isValidEmailAddress(user.getUsername())) {
-			throw new UserException("Users must have a valid e-mail address to checkin");
-		}
-		checkCheckSum(project);
-		if (!project.getRevisions().isEmpty() && project.getRevisions().get(project.getRevisions().size()-1).getState() == CheckinState.STORING) {
-			throw new UserException("Another checkin on this project is currently running, please wait and try again");
-		}
-		ConcreteRevision concreteRevision = createNewConcreteRevision(getDatabaseSession(), getModel().getSize(), poid, actingUid, comment.trim(), CheckinState.STORING);
-		concreteRevision.setChecksum(getModel().getChecksum());
-		NewRevisionAdded newRevisionAdded = LogFactory.eINSTANCE.createNewRevisionAdded();
-		newRevisionAdded.setDate(new Date());
-		newRevisionAdded.setExecutor(user);
-		newRevisionAdded.setRevision(concreteRevision.getRevisions().get(0));
-		newRevisionAdded.setAccessMethod(getAccessMethod());
-		getDatabaseSession().store(newRevisionAdded);
-		getDatabaseSession().store(concreteRevision);
-		getDatabaseSession().store(project);
-		return concreteRevision;
 	}
 }
