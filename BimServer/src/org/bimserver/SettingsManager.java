4716848041db3f4f97203df6c3d23bee3a37df68
Ruben de Laat <ruben@logic-labs.nl>
2011-11-02 15:01:04 +0000

    http://code.google.com/p/bimserver/issues/detail?id=315

--- BimServer/src/org/bimserver/SettingsManager.java
+++ BimServer/src/org/bimserver/SettingsManager.java
@@ -56,7 +56,7 @@ public class SettingsManager {
 		settings.setSmtpServer("");
 		settings.setRegistrationAddition("");
 		settings.setHeaderAddition("");
-		settings.setFooterAddition("");
+		settings.setFooterAddition("<script type=\"text/javascript\">var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-12034708-3']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>");
 		return settings;
 	}
 	
