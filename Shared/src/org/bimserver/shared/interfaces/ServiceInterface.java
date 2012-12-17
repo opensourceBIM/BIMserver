31acac7f0eeefc2bddd6461614bbbdde3d54391d
Ruben de Laat <ruben@logic-labs.nl>
2012-12-17 16:13:50 +0000

    Removed redundant objects

--- Shared/src/org/bimserver/shared/interfaces/ServiceInterface.java
+++ Shared/src/org/bimserver/shared/interfaces/ServiceInterface.java
@@ -2217,6 +2217,11 @@ public interface ServiceInterface extends PublicInterface {
 		@WebParam(name = "roid", partName = "triggerNewRevision.roid") Long roid, 
 		@WebParam(name = "soid", partName = "triggerNewRevision.soid") Long soid) throws ServerException, UserException;
 
+	@WebMethod(action = "triggerNewExtendedData")
+	void triggerNewExtendedData(
+		@WebParam(name = "edid", partName = "triggerNewExtendedData.edid") Long edid, 
+		@WebParam(name = "soid", partName = "triggerNewExtendedData.soid") Long soid) throws ServerException, UserException;
+
 	@WebMethod(action = "getAllLocalServiceDescriptors")
 	List<SServiceDescriptor> getAllLocalServiceDescriptors() throws ServerException, UserException;
 
