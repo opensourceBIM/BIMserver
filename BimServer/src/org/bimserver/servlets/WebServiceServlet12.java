412fe1a8991530b63d51e3717a4d2fc84d6416d4
Ruben de Laat <ruben@logic-labs.nl>
2013-04-02 07:46:45 +0000

    Added support for SOAP 1.2

---
+++ BimServer/src/org/bimserver/servlets/WebServiceServlet12.java
@@ -0,0 +1,29 @@
+package org.bimserver.servlets;
+
+import org.apache.cxf.common.WSDLConstants;
+
+/******************************************************************************
+ * Copyright (C) 2009-2013  BIMserver.org
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+public class WebServiceServlet12 extends GenericWebServiceServlet {
+
+	private static final long serialVersionUID = 8887636178745098071L;
+	
+	public WebServiceServlet12() {
+		setBindingId(WSDLConstants.NS_SOAP12);
+	}
+}
