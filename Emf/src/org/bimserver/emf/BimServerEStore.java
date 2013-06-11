96c1cbcba067c0b386e20a9128617997acdba547
Ruben de Laat <ruben@logic-labs.nl>
2013-06-11 10:41:18 +0000

    This is merged with the Shared project

--- Emf/src/org/bimserver/emf/BimServerEStore.java
+++
@@ -1,8 +0,0 @@
-package org.bimserver.emf;
-
-import org.eclipse.emf.ecore.InternalEObject.EStore;
-
-public interface BimServerEStore extends EStore {
-	void load(IdEObject object);
-	void remove(IdEObject object);
-}
