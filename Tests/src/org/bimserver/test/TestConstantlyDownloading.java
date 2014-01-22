293e1972682992dd6ca2eec68390d3dbe8077664
Ruben de Laat <ruben@logic-labs.nl>
2014-01-22 13:50:47 +0100

    Updated license texts to 2014

---
+++ Tests/src/org/bimserver/test/TestConstantlyDownloading.java
@@ -0,0 +1,53 @@
+package org.bimserver.test;
+
+/******************************************************************************
+ * Copyright (C) 2009-2014  BIMserver.org
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+import java.io.ByteArrayOutputStream;
+
+import org.bimserver.client.json.JsonBimServerClientFactory;
+import org.bimserver.interfaces.objects.SProject;
+import org.bimserver.interfaces.objects.SSerializerPluginConfiguration;
+import org.bimserver.plugins.services.BimServerClientInterface;
+import org.bimserver.shared.ChannelConnectionException;
+import org.bimserver.shared.PublicInterfaceNotFoundException;
+import org.bimserver.shared.UsernamePasswordAuthenticationInfo;
+import org.bimserver.shared.exceptions.ServiceException;
+
+public class TestConstantlyDownloading {
+	public static void main(String[] args) {
+		long poid = 196609;		
+		
+		JsonBimServerClientFactory factory = new JsonBimServerClientFactory("http://localhost:8080");
+		try {
+			BimServerClientInterface client = factory.create(new UsernamePasswordAuthenticationInfo("admin@bimserver.org", "admin"));
+			for (int i=0; i<200; i++) {
+				SProject project = client.getBimsie1ServiceInterface().getProjectByPoid(poid);
+				long roid = project.getLastRevisionId();
+				SSerializerPluginConfiguration serializer = client.getPluginInterface().getSerializerByPluginClassName("org.bimserver.geometry.json.JsonGeometrySerializerPlugin");
+				System.out.println(i);
+				client.download(roid, serializer.getOid(), new ByteArrayOutputStream());
+			}
+		} catch (ServiceException e) {
+			e.printStackTrace();
+		} catch (ChannelConnectionException e) {
+			e.printStackTrace();
+		} catch (PublicInterfaceNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
