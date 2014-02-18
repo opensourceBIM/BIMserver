b3cb7c9c890aed047098ba78c414f239de30768c
Ruben de Laat <ruben@logic-labs.nl>
2014-02-18 16:40:35 +0100

    Fixed bug when setting wrappedvalue of new object, still a hack...

--- Tests/src/org/bimserver/test/TestChangeWrappedValue.java
+++ Tests/src/org/bimserver/test/TestChangeWrappedValue.java
@@ -3,9 +3,9 @@ package org.bimserver.test;
 import org.bimserver.client.BimServerClient;
 import org.bimserver.client.ClientIfcModel;
 import org.bimserver.client.json.JsonBimServerClientFactory;
+import org.bimserver.emf.IfcModelInterfaceException;
 import org.bimserver.models.ifc2x3tc1.IfcLabel;
 import org.bimserver.models.ifc2x3tc1.IfcPropertySingleValue;
-import org.bimserver.models.ifc2x3tc1.IfcValue;
 import org.bimserver.plugins.services.BimServerClientException;
 import org.bimserver.shared.ChannelConnectionException;
 import org.bimserver.shared.PublicInterfaceNotFoundException;
@@ -21,16 +21,20 @@ public class TestChangeWrappedValue {
 		JsonBimServerClientFactory factory = new JsonBimServerClientFactory("http://localhost:8080");
 		try {
 			BimServerClient client = factory.create(new UsernamePasswordAuthenticationInfo("admin@bimserver.org", "admin"));
-			long poid = 131073;
-			long roid = 131075;
+			long poid = 2686977;
+			long roid = 720899;
 			ClientIfcModel model = client.getModel(poid, roid, true);
 			
 			for (IfcPropertySingleValue prop : model.getAll(IfcPropertySingleValue.class)) {
-				IfcValue value = ((IfcPropertySingleValue) prop).getNominalValue();
-				if(value instanceof IfcLabel){
-					System.out.println(prop.getOid() + " is " + ((IfcLabel) value).getWrappedValue() );
-		    	   ((IfcLabel) value).setWrappedValue(((IfcLabel) value).getWrappedValue() + " changed");
-				}
+//				IfcValue value = ((IfcPropertySingleValue) prop).getNominalValue();
+//				if(value instanceof IfcLabel){
+//					System.out.println(prop.getOid() + " is " + ((IfcLabel) value).getWrappedValue() );
+//		    	   ((IfcLabel) value).setWrappedValue(((IfcLabel) value).getWrappedValue() + " changed");
+//				}
+				
+				IfcLabel label = model.create(IfcLabel.class);
+				label.setWrappedValue("blabla");
+				prop.setNominalValue(label);
 			}
 			
 			model.commit("blaat");
@@ -39,6 +43,8 @@ public class TestChangeWrappedValue {
 		} catch (BimServerClientException e) {
 			e.printStackTrace();
 		} catch (PublicInterfaceNotFoundException e) {
+			e.printStackTrace();
+		} catch (IfcModelInterfaceException e) {
 			e.printStackTrace();
 		}
 	}
