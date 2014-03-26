c6e7a28d23b696f7e77b51e21c0d1a1f43ed9f2d
Ruben de Laat <ruben@logic-labs.nl>
2014-03-26 21:36:07 +0100

    Playing with IFC4

--- BuildingSMARTLibrary/src/nl/tue/buildingsmart/emf/Ifc4.java
+++ BuildingSMARTLibrary/src/nl/tue/buildingsmart/emf/Ifc4.java
@@ -19,9 +19,9 @@ package nl.tue.buildingsmart.emf;
 
 import java.io.File;
 
-public class Ifc2x4rc3 {
+public class Ifc4 {
 	public static void main(String[] args) {
-		Express2EMF express2emf = new Express2EMF(new File("schema/IFC2X4_RC3.exp"), "ifc2x4rc3");
-		express2emf.writeEMF("../BimServer/src/org/bimserver/database/migrations/steps/" + "IFC2X4_RC3.ecore");
+		Express2EMF express2emf = new Express2EMF(new File("schema/IFC4.exp"), "ifc4");
+		express2emf.writeEMF("../BimServer/src/org/bimserver/database/migrations/steps/" + "IFC4.ecore");
 	}
 }
