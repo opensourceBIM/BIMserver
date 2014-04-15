6e4cdec2288249a4697f409d0915a30d563093fd
Ruben de Laat <ruben@logic-labs.nl>
2014-04-15 15:38:43 +0200

    updated jquery

--- BimServer/www/js/bimserverapibootstrap.js
+++ BimServer/www/js/bimserverapibootstrap.js
@@ -1,30 +1,37 @@
 function loadBimServerApi(address, notifier, callback, errorCallback) {
 	var timeoutId = window.setTimeout(function() {
-		notifier.error("Could not connect");
+		notifier.setError("Could not connect");
 		errorCallback();
 	}, 3000);
+	if (address.endsWith("/")) {
+		address = address.substring(0, address.length - 1);
+	}
 	$.getScript(address + "/js/bimserverapi.js").done(function(){
 		window.clearTimeout(timeoutId);
 		if (typeof BimServerApi != 'function') {
-			notifier.error("Could not connect");
+			notifier.setError("Could not connect");
 			errorCallback();
 		} else {
 			if (BimServerApi != null) {
 				Global.bimServerApi = new BimServerApi(address, notifier);
 				Global.bimServerApi.init(function(){
+					// TODO make 1 call
 					Global.bimServerApi.call("AdminInterface", "getServerInfo", {}, function(serverInfo){
-						callback(serverInfo);
+						Global.bimServerApi.call("AdminInterface", "getVersion", {}, function(version){
+							Global.bimServerApi.version = version;
+							callback(serverInfo);
+						});
 					});
 				});
 			} else {
 				window.clearTimeout(timeoutId);
-				notifier.error("Could not connect");
+				notifier.setError("Could not find BIMserver API");
 				errorCallback();
-			}				
+			}
 		}
 	}).fail(function(jqxhr, settings, exception){
 		window.clearTimeout(timeoutId);
-		notifier.error("Could not connect");
+		notifier.setError("Could not connect");
 		errorCallback();
 	});
 }
