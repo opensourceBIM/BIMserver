b23f80e35790af984f81d0f20af916f3f7d2dc6e
Ruben de Laat <ruben@logic-labs.nl>
2014-06-17 13:03:01 +0200

    trying commit before merge

--- Shared/generated/org/bimserver/interfaces/objects/SUserRelated.java
+++ Shared/generated/org/bimserver/interfaces/objects/SUserRelated.java
@@ -24,7 +24,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 
 @XmlRootElement
-@XmlSeeAlso(value={SUserUndeleted.class, SUserChanged.class, SPasswordChanged.class, SPasswordReset.class, SNewUserAdded.class, SUserAddedToProject.class, SUserRemovedFromProject.class, SUserDeleted.class})
+@XmlSeeAlso(value={SUserUndeleted.class, SPasswordReset.class, SPasswordChanged.class, SUserRemovedFromProject.class, SUserAddedToProject.class, SUserChanged.class, SNewUserAdded.class, SUserDeleted.class})
 public class SUserRelated extends SLogAction implements SDataBase
 {
 	private long oid = -1;
