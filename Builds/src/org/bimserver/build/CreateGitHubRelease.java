950acabd1722739b776c819a506e1edd470bd753
Ruben de Laat <ruben@logic-labs.nl>
2014-11-19 11:27:12 +1300

    trying to work around githubs ssl certificate that seems to be wrong

--- Builds/src/org/bimserver/build/CreateGitHubRelease.java
+++ Builds/src/org/bimserver/build/CreateGitHubRelease.java
@@ -20,6 +20,9 @@ package org.bimserver.build;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,13 +31,19 @@ import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.AuthCache;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.eclipse.egit.github.core.client.GitHubClient;
 
 import com.google.gson.JsonObject;
@@ -72,10 +81,15 @@ public class CreateGitHubRelease {
 			
 			HttpHost httpHost = new HttpHost("uploads.github.com", 443, "https");
 
-			DefaultHttpClient client = new DefaultHttpClient();
 			BasicCredentialsProvider basicCredentialsProvider = new BasicCredentialsProvider();
 			basicCredentialsProvider.setCredentials(new AuthScope(httpHost), new UsernamePasswordCredentials(username, password));
-			client.setCredentialsProvider(basicCredentialsProvider);
+
+			SSLContextBuilder builder = new SSLContextBuilder();
+			    builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+			    SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+			            builder.build());
+			    CloseableHttpClient client = HttpClients.custom().setDefaultCredentialsProvider(basicCredentialsProvider).setSSLSocketFactory(
+			            sslsf).build();
 			
 			AuthCache authCache = new BasicAuthCache();
 			BasicScheme basicAuth = new BasicScheme();
@@ -96,6 +110,12 @@ public class CreateGitHubRelease {
 				execute.getEntity().getContent().close();
 			}
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+		} catch (KeyManagementException e) {
 			e.printStackTrace();
 		}
 	}
