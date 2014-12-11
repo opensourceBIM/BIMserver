34b8fa201469de3773d206436e2f6a82854c998f
Marcin Koziuk <marcin@irp.nl>
2014-12-11 15:36:17 +0100

    Change the package of the Record class in the Shared module
    The current situation causes "java.lang.IncompatibleClassChangeError: Implementing class" due to a different class with the same name and package already being present inside the BimServer module.

--- Shared/src/org/bimserver/shared/SingleRecordWaitingObject.java
+++ Shared/src/org/bimserver/shared/SingleRecordWaitingObject.java
@@ -1,6 +1,5 @@
 package org.bimserver.shared;
 
-import org.bimserver.database.Record;
 import org.eclipse.emf.ecore.EStructuralFeature;
 
 public class SingleRecordWaitingObject extends RecordWaitingObject{
