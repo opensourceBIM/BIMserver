0a4697275adebb41c6d680f7f24bbe503b38de34
Ruben de Laat <ruben@logic-labs.nl>
2015-02-17 11:25:11 +1300

    Added pmd

--- BimServer/src/org/bimserver/database/actions/GetDataObjectsDatabaseAction.java
+++ BimServer/src/org/bimserver/database/actions/GetDataObjectsDatabaseAction.java
@@ -64,6 +64,7 @@ public class GetDataObjectsDatabaseAction extends AbstractDownloadDatabaseAction
 		for (ConcreteRevision concreteRevision : virtualRevision.getConcreteRevisions()) {
 			int highestStopId = findHighestStopRid(concreteRevision.getProject(), concreteRevision);
 			PackageMetaData packageMetaData = getBimServer().getMetaDataManager().getPackageMetaData(concreteRevision.getProject().getSchema());
+			lastPackageMetaData = packageMetaData;
 			IfcModel subModel = new IfcModel(packageMetaData, pidRoidMap);
 			Query query = new Query(packageMetaData, concreteRevision.getProject().getId(), concreteRevision.getId(), virtualRevision.getOid(), null, Deep.YES, highestStopId);
 			getDatabaseSession().getMap(subModel, query);
