27811efd2b970f26b3b1dbb43594962e7e644993
Ruben de Laat <ruben@logic-labs.nl>
2015-02-17 11:05:47 +1300

    Disabled log

--- BimServerClientLib/src/org/bimserver/client/ClientIfcModel.java
+++ BimServerClientLib/src/org/bimserver/client/ClientIfcModel.java
@@ -245,7 +245,7 @@ public class ClientIfcModel extends IfcModel {
 		try {
 			InputStream downloadData = bimServerClient.getDownloadData(download, getIfcSerializerOid());
 			try {
-				boolean log = true;
+				boolean log = false;
 				// TODO Make this streaming again, make sure the EmfSerializer getInputStream method is working properly
 				if (log) {
 					ByteArrayOutputStream baos = new ByteArrayOutputStream();
