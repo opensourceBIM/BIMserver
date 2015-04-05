61a227c458ce5cb7a0819f938d3f08858d9fe136
Ruben de Laat <ruben@logic-labs.nl>
2015-04-05 10:41:09 +0200

    Added documentation

--- BimServer/src/org/bimserver/TriggerImportDataRemote.java
+++ BimServer/src/org/bimserver/TriggerImportDataRemote.java
@@ -31,13 +31,21 @@ import org.bimserver.shared.exceptions.ServiceException;
 
 public class TriggerImportDataRemote {
 	public static void main(String[] args) {
+		/*
+		 * Args:
+		 * 	0: Address of new server
+		 *  1: Username
+		 *  2: Password
+		 *  3: Address of old server
+		 *  4: Local path to incoming folder of old server, this local path has to be available on the new server, so you have to copy it from the old server
+		 */
 		try {
 			PluginManager pluginManager = LocalDevPluginLoader.createPluginManager(new File("home"));
 			MetaDataManager metaDataManager = new MetaDataManager(pluginManager);
 			pluginManager.setMetaDataManager(metaDataManager);
 			JsonBimServerClientFactory factory = new JsonBimServerClientFactory(metaDataManager, args[0]);
 			BimServerClient client = factory.create(new UsernamePasswordAuthenticationInfo(args[1], args[2]));
-			client.getServiceInterface().importData(args[3], args[1], args[2], "/var/incoming");
+			client.getServiceInterface().importData(args[3], args[1], args[2], args[4]);
 		} catch (PluginException e) {
 			e.printStackTrace();
 		} catch (ServiceException e) {
