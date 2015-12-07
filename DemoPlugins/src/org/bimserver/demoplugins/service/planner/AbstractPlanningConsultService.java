fcc70d0b03170e8364bb1c25f141519cd50577b2
Ruben de Laat <ruben@logic-labs.nl>
2015-12-07 16:27:35 +0100

    Planning consult now applying simplification of material names to eventlog as well

--- DemoPlugins/src/org/bimserver/demoplugins/service/planner/AbstractPlanningConsultService.java
+++ DemoPlugins/src/org/bimserver/demoplugins/service/planner/AbstractPlanningConsultService.java
@@ -52,7 +52,8 @@ public abstract class AbstractPlanningConsultService extends AbstractAddExtended
 					materialAggregators.add(pluginConfiguration.getString("materialaggregator" + i));
 				}
 
-				planner.feedTrainingData(new EventLog(new ByteArrayInputStream(eventLogBytes)));
+				EventLog eventLog = new EventLog(new ByteArrayInputStream(eventLogBytes), materialAggregators);
+				planner.feedTrainingData(eventLog);
 				planner.analyze(materialAggregators);
 			}
 
