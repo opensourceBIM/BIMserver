7d62c89f0363a34febf894435772648516ed3a1f
Ruben de Laat <ruben@logic-labs.nl>
2016-01-12 12:19:27 +0100

    disable strict javadoc checking for now

--- IfcPlugins/src/org/bimserver/ifc/step/serializer/IfcStepSerializerPlugin.java
+++ IfcPlugins/src/org/bimserver/ifc/step/serializer/IfcStepSerializerPlugin.java
@@ -14,7 +14,7 @@ package org.bimserver.ifc.step.serializer;
  * GNU Affero General Public License for more details.
  * 
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see {@literal<http://www.gnu.org/licenses/>}.
  *****************************************************************************/
 
 import org.bimserver.models.store.ObjectDefinition;
--- Tests/src/org/bimserver/test/framework/actions/CreateProjectAction.java
+++ Tests/src/org/bimserver/test/framework/actions/CreateProjectAction.java
@@ -14,7 +14,7 @@ package org.bimserver.test.framework.actions;
  * GNU Affero General Public License for more details.
  * 
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see {@literal<http://www.gnu.org/licenses/>}.
  *****************************************************************************/
 
 import org.bimserver.shared.exceptions.PublicInterfaceNotFoundException;
