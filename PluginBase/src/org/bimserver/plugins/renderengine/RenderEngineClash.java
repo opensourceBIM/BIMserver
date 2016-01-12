829f3d7526b4021b4af82d358df6603cd91317bd
Ruben de Laat <ruben@logic-labs.nl>
2016-01-12 12:05:03 +0100

    maven + licenses

--- PluginBase/src/org/bimserver/plugins/renderengine/RenderEngineClash.java
+++ PluginBase/src/org/bimserver/plugins/renderengine/RenderEngineClash.java
@@ -14,7 +14,7 @@ package org.bimserver.plugins.renderengine;
  * GNU Affero General Public License for more details.
  * 
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see {@literal<http://www.gnu.org/licenses/>}.
  *****************************************************************************/
 
 public class RenderEngineClash {
