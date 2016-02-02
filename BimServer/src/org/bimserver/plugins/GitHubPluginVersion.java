3617b080cbd5ec2bf3cd00af741713aee45e05e2
Ruben de Laat <ruben@logic-labs.nl>
2016-02-02 15:42:38 +0100

    new files

---
+++ BimServer/src/org/bimserver/plugins/GitHubPluginVersion.java
@@ -0,0 +1,20 @@
+package org.bimserver.plugins;
+
+public class GitHubPluginVersion extends PluginVersion {
+
+	public String getVersion() {
+		return null;
+	}
+
+	public String getDescription() {
+		return null;
+	}
+
+	public String getName() {
+		return null;
+	}
+
+	public String getOrganization() {
+		return null;
+	}
+}
