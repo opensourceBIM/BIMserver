bd55519e9f9a53ce37eb472b4d1dcbeacdd262ef
Ruben de Laat <ruben@logic-labs.nl>
2016-03-11 12:36:08 +0100

    Updated internal deps to 1.5.34-SNAPSHOT

---
+++ PluginBase/src/org/bimserver/geometry/IfcColors.java
@@ -0,0 +1,46 @@
+package org.bimserver.geometry;
+
+public class IfcColors {
+	public static float[] getDefaultColor(String type) {
+		type = type.toUpperCase();
+		switch (type) {
+			case "IFCROOF": return new float[]{0.837255f, 0.203922f, 0.270588f, 1.0f};
+			case "IFCSLAB": return new float[]{0.637255f, 0.603922f, 0.670588f, 1.0f};
+			case "IFCWALL": return new float[]{0.537255f, 0.337255f, 0.237255f, 1.0f};
+			case "IFCWALLSTANDARDCASE": return new float[]{0.537255f, 0.337255f, 0.237255f, 1.0f};
+			case "IFCDOOR": return new float[]{0.637255f, 0.603922f, 0.670588f, 1.0f};
+			case "IFCWINDOW": return new float[]{0.137255f, 0.403922f, 0.870588f, 0.5f};
+			case "IFCOPENINGELEMENT": return new float[]{0.137255f, 0.403922f, 0.870588f, 0f};
+			case "IFCRAILING": return new float[]{0.137255f, 0.403922f, 0.870588f, 1.0f};
+			case "IFCOLUMN": return new float[]{0.137255f, 0.403922f, 0.870588f, 1.0f};
+			case "IFCBEAM": return new float[]{0.137255f, 0.403922f, 0.870588f, 1.0f};
+			case "IFCFURNISHINGELEMENT": return new float[]{0.137255f, 0.403922f, 0.870588f, 1.0f};
+			case "IFCCURTAINWALL": return new float[]{0.137255f, 0.403922f, 0.870588f, 1.0f};
+			case "IFCSTAIR": return new float[]{0.637255f, 0.603922f, 0.670588f, 1.0f};
+			case "IFCSTAIRFLIGHT": return new float[]{0.637255f, 0.603922f, 0.670588f, 1.0f};
+			case "IFCBUILDINGELEMENTPROXY": return new float[]{0.5f, 0.5f, 0.5f, 1.0f};
+			case "IFCFLOWSEGMENT": return new float[]{0.137255f, 0.403922f, 0.870588f, 1.0f};
+			case "IFCFLOWFITTING": return new float[]{0.137255f, 0.403922f, 0.870588f, 1.0f};
+			case "IFCFLOWTERMINAL": return new float[]{0.137255f, 0.403922f, 0.870588f, 1.0f};
+			case "IFCPROY": return new float[]{0.137255f, 0.403922f, 0.870588f, 1.0f};
+			case "IFCSITE": return new float[]{0.137255f, 0.403922f, 0.870588f, 1.0f};
+			case "IFCLIGHTFIXTURE": return new float[]{0.8470588235f, 0.8470588235f, 0.870588f, 1.0f};
+			case "IFCDUCTSEGMENT": return new float[]{0.8470588235f, 0.427450980392f, 0f, 1.0f};
+			case "IFCDISTRIBUTIONFLOWELEMENT": return new float[]{0.8470588235f, 0.427450980392f, 0f, 1.0f};
+			case "IFCDUCTFITTING": return new float[]{0.8470588235f, 0.427450980392f, 0f, 1.0f};
+			case "IFCPLATE": return new float[]{0.8470588235f, 0.427450980392f, 0f, 1.0f};
+			case "IFCAIRTERMINAL": return new float[]{0.8470588235f, 0.427450980392f, 0f, 1.0f};
+			case "IFCMEMBER": return new float[]{0.8470588235f, 0.427450980392f, 0f, 1.0f};
+			case "IFCCOVERING": return new float[]{0.8470588235f, 0.427450980392f, 0f, 1.0f};
+			case "IFCTRANSPORTELEMENT": return new float[]{0.8470588235f, 0.427450980392f, 0f, 1.0f};
+			case "IFCFLOWCONTROLLER": return new float[]{0.8470588235f, 0.427450980392f, 0f, 1.0f};
+			case "IFCRAMP": return new float[]{0.8470588235f, 0.427450980392f, 0f, 1.0f};
+			case "IFCFURNITURE": return new float[]{0.8470588235f, 0.427450980392f, 0f, 1.0f};
+			case "IFCFOOTING": return new float[]{0.8470588235f, 0.427450980392f, 0f, 1.0f};
+			case "IFCSYSTEMFURNITUREELEMENT": return new float[]{0.8470588235f, 0.427450980392f, 0f, 1.0f};
+			case "IFCSPACE": return new float[]{0.137255f, 0.303922f, 0.570588f, 0.5f};
+			case "IFCBUILDINGELEMENTPART": return new float[]{1f, 0.5f, 0.5f, 1.0f};
+		}
+		return new float[]{0.8470588235f, 0.427450980392f, 0f, 1f};
+	}
+}
