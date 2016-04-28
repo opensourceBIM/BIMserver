ee0542ea7b25854d00a5583936b6e1d94ee796f2
Ruben de Laat <ruben@logic-labs.nl>
2016-04-28 18:55:50 +0200

    Updated default queries

--- PluginBase/generated/org/bimserver/interfaces/objects/SPrimitiveType.java
+++ PluginBase/generated/org/bimserver/interfaces/objects/SPrimitiveType.java
@@ -23,7 +23,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 
 @XmlRootElement
-@XmlSeeAlso(value={SDoubleType.class, SLongType.class, SStringType.class, SBooleanType.class, SByteArrayType.class})
+@XmlSeeAlso(value={SStringType.class, SDoubleType.class, SByteArrayType.class, SLongType.class, SBooleanType.class})
 public class SPrimitiveType extends SType implements SDataBase
 {
 	private long oid = -1;
