46544a738df1bdb4137e1ebb72a42ef088cf4367
Ruben de Laat <ruben@logic-labs.nl>
2016-05-19 08:52:22 +0200

    Added more types to default queries to fix streaming geometry generation for IfcFaceBasedSurfaceModel types

---
+++ BimServer/src/org/bimserver/GenericGeometryGenerator.java
@@ -0,0 +1,323 @@
+package org.bimserver;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.util.Random;
+
+import org.bimserver.geometry.Matrix;
+import org.bimserver.geometry.Vector;
+
+public class GenericGeometryGenerator {
+	private static boolean almostTheSame(float f1, float f2, float maxDiff) {
+		return Math.abs(f1 - f2) < maxDiff;
+	}
+
+	/**
+	 * This function should return a transformation matrix (with translation and
+	 * rotation, no scaling) overlaying triangle V on U Assumed is that the
+	 * triangles are indeed the same and the order of the vertices is also the
+	 * same (shifts are not allowed) This function can probably be optimized for
+	 * speed and also the LOC can probably be reduced
+	 * 
+	 * @param originalV1
+	 * @param originalV2
+	 * @param originalV3
+	 * @param u1
+	 * @param u2
+	 * @param u3
+	 * @return
+	 */
+	private static float[] getTransformationMatrix(float[] originalV1, float[] originalV2, float[] originalV3, float[] u1, float[] u2, float[] u3, float maxDiff) {
+		float[] v1 = copy(originalV1);
+		float[] v2 = copy(originalV2);
+		float[] v3 = copy(originalV3);
+		u1 = copy(u1);
+		u2 = copy(u2);
+		u3 = copy(u3);
+
+		float transX = u1[0] - v1[0];
+		float transY = u1[1] - v1[1];
+		float transZ = u1[2] - v1[2];
+
+		float translation[] = new float[16];
+		Matrix.setIdentityM(translation, 0);
+		Matrix.translateM(translation, 0, u1[0], u1[1], u1[2]);
+
+		float[] toZeroTranslation = new float[16];
+		Matrix.setIdentityM(toZeroTranslation, 0);
+		Matrix.translateM(toZeroTranslation, 0, -v1[0], -v1[1], -v1[2]);
+
+		if (almostTheSame(v2[0] + transX, u2[0], maxDiff) && almostTheSame(v2[1] + transY, u2[1], maxDiff) && almostTheSame(v2[2] + transZ, u2[2], maxDiff)
+				&& almostTheSame(v3[0] + transX, u3[0], maxDiff) && almostTheSame(v3[1] + transY, u3[1], maxDiff) && almostTheSame(v3[2] + transZ, u3[2], maxDiff)) {
+			// The other two points are already the same, to there was no
+			// rotation
+			return translation;
+		}
+
+		// Normalize both triangles to their first vertex
+		subtract(u2, u1);
+		subtract(u3, u1);
+		subtract(u1, u1);
+
+		subtract(v2, v1);
+		subtract(v3, v1);
+		subtract(v1, v1);
+
+		float[] u2CrossV2 = Vector.crossProduct(u2, v2);
+		float[] r2 = new float[16];
+		Matrix.setIdentityM(r2, 0);
+		float[] r2v2 = new float[4];
+		if (!equalsAlmost(u2, v2, maxDiff)) {
+			float u2InV2 = Vector.dot(u2, v2);
+			float[] axis = u2CrossV2;
+			if (axis[0] == 0 && axis[1] == 0 && axis[2] == 0) {
+				axis = new float[] { u2[1], -u2[0], 0, 0 };
+			}
+			Matrix.rotateM(r2, 0, (float) Math.toDegrees(Math.atan2(Vector.length(u2CrossV2), u2InV2)), axis[0], axis[1], axis[2]);
+
+			Matrix.multiplyMV(r2v2, 0, r2, 0, new float[] { v2[0], v2[1], v2[2], 1 }, 0);
+
+			if (!equalsAlmost(r2v2, u2, maxDiff)) {
+				Matrix.setIdentityM(r2, 0);
+				Matrix.rotateM(r2, 0, -(float) Math.toDegrees(Math.atan2(Vector.length(u2CrossV2), u2InV2)), axis[0], axis[1], axis[2]);
+				Matrix.multiplyMV(r2v2, 0, r2, 0, new float[] { v2[0], v2[1], v2[2], 1 }, 0);
+				if (!equalsAlmost(r2v2, u2, maxDiff)) {
+					return null;
+				}
+			}
+		} else {
+			r2v2 = copy(v2);
+		}
+
+		float[] r2v3 = new float[4];
+		Matrix.multiplyMV(r2v3, 0, r2, 0, new float[] { v3[0], v3[1], v3[2], 1 }, 0);
+
+		float[] r3 = new float[16];
+		Matrix.setIdentityM(r3, 0);
+
+		float angleDegrees = (float) Math.toDegrees(getPlaneAngle(v1, r2v2, r2v3, u1, u2, u3));
+
+		Matrix.rotateM(r3, 0, angleDegrees, r2v2[0], r2v2[1], r2v2[2]);
+
+		float[] r3v3 = new float[4];
+		Matrix.multiplyMV(r3v3, 0, r3, 0, new float[] { r2v3[0], r2v3[1], r2v3[2], 1 }, 0);
+
+		float[] r2v1 = new float[4];
+		Matrix.multiplyMV(r2v1, 0, r2, 0, new float[] { v1[0], v1[1], v1[2], 1 }, 0);
+
+		float[] r3v1 = new float[4];
+		Matrix.multiplyMV(r3v1, 0, r3, 0, new float[] { r2v1[0], r2v1[1], r2v1[2], 1 }, 0);
+
+		if (!equalsAlmost(r3v3, u3, maxDiff)) {
+			Matrix.setIdentityM(r3, 0);
+			Matrix.rotateM(r3, 0, -angleDegrees, r2v2[0], r2v2[1], r2v2[2]);
+			Matrix.multiplyMV(r3v3, 0, r3, 0, new float[] { r2v3[0], r2v3[1], r2v3[2], 1 }, 0);
+			float[] r3v2 = new float[4];
+			Matrix.multiplyMV(r3v2, 0, r3, 0, new float[] { r2v2[0], r2v2[1], r2v2[2], 1 }, 0);
+			if (!equalsAlmost(r3v3, u3, maxDiff) || !equalsAlmost(r3v2, u2, maxDiff)) {
+				return null;
+			}
+		}
+		float[] subResult = new float[16];
+		float[] subResult2 = new float[16];
+		float[] subResult3 = new float[16];
+		float[] totalResult = new float[16];
+		float[] startMatrix = new float[16];
+		Matrix.setIdentityM(startMatrix, 0);
+
+		Matrix.multiplyMM(subResult, 0, toZeroTranslation, 0, startMatrix, 0);
+		Matrix.multiplyMM(subResult2, 0, r2, 0, subResult, 0);
+		Matrix.multiplyMM(subResult3, 0, r3, 0, subResult2, 0);
+		Matrix.multiplyMM(totalResult, 0, translation, 0, subResult3, 0);
+
+		return totalResult;
+	}
+
+	/**
+	 * Get the angle in radians between two planes
+	 * 
+	 * @param v1
+	 * @param v2
+	 * @param v3
+	 * @param u1
+	 * @param u2
+	 * @param u3
+	 * @return
+	 */
+	private static double getPlaneAngle(float[] v1, float[] v2, float[] v3, float[] u1, float[] u2, float[] u3) {
+		float[] cross1 = Vector.crossProduct(new float[] { v2[0] - v1[0], v2[1] - v1[1], v2[2] - v1[2] }, new float[] { v3[0] - v1[0], v3[1] - v1[1], v3[2] - v1[2] });
+		float[] cross2 = Vector.crossProduct(new float[] { u2[0] - u1[0], u2[1] - u1[1], u2[2] - u1[2] }, new float[] { u3[0] - u1[0], u3[1] - u1[1], u3[2] - u1[2] });
+
+		float num = Vector.dot(cross1, cross2);
+		float den = Vector.length(cross1) * Vector.length(cross2);
+
+		float a = num / den;
+		if (a > 1) {
+			a = 1;
+		}
+		if (a < -1) {
+			a = -1;
+		}
+		double result = Math.acos(a);
+
+		if (Double.isNaN(result)) {
+			System.out.println();
+		}
+		return result;
+	}
+
+	private static boolean equalsAlmost(float[] r2v2, float[] u2, float maxDiff) {
+		for (int i = 0; i < 3; i++) {
+			if (!almostTheSame(r2v2[i], u2[i], maxDiff)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static float[] copy(float[] v1) {
+		float[] result = new float[v1.length];
+		System.arraycopy(v1, 0, result, 0, v1.length);
+		return result;
+	}
+
+	private static boolean test(float[] v1, float[] v2, float[] transformationMatrix, float maxDiff) {
+		float[] resultVector = new float[4];
+		Matrix.multiplyMV(resultVector, 0, transformationMatrix, 0, new float[] { v1[0], v1[1], v1[2], 1 }, 0);
+		normalize(resultVector);
+		boolean theSame = true;
+		for (int i = 0; i < 3; i++) {
+			if (!almostTheSame(resultVector[i], v2[i], maxDiff)) {
+				theSame = false;
+			}
+		}
+		if (!theSame) {
+			System.out.println("Difference");
+			Vector.dump("Was", v1);
+			Vector.dump("Became", resultVector);
+			Vector.dump("Should be", v2);
+			System.out.println();
+			return false;
+		}
+		return true;
+	}
+
+	private static void normalize(float[] resultVector) {
+		resultVector[0] = resultVector[0] * resultVector[3];
+		resultVector[1] = resultVector[1] * resultVector[3];
+		resultVector[2] = resultVector[2] * resultVector[3];
+		resultVector[3] = 1;
+	}
+
+	private static void subtract(float[] u2, float[] v1) {
+		u2[0] = u2[0] - v1[0];
+		u2[1] = u2[1] - v1[1];
+		u2[2] = u2[2] - v1[2];
+	}
+
+	public static void main(String[] args) {
+		float maxDiff = 0.1f;
+
+		test1(maxDiff);
+		test2(maxDiff);
+
+		Random random = new Random();
+		for (int i = 0; i < 10; i++) {
+			float[] matrix = new float[16];
+			Matrix.setIdentityM(matrix, 0);
+			for (int j = 0; j < 10; j++) {
+				Matrix.rotateM(matrix, 0, random.nextFloat() * 360, random.nextFloat(), random.nextFloat(), random.nextFloat());
+			}
+
+			float[] v1 = new float[] { random.nextFloat(), random.nextFloat(), random.nextFloat(), 1 };
+			float[] v2 = new float[] { random.nextFloat(), random.nextFloat(), random.nextFloat(), 1 };
+			float[] v3 = new float[] { random.nextFloat(), random.nextFloat(), random.nextFloat(), 1 };
+			float[] r1 = new float[4];
+			float[] r2 = new float[4];
+			float[] r3 = new float[4];
+			Matrix.multiplyMV(r1, 0, matrix, 0, v1, 0);
+			Matrix.multiplyMV(r2, 0, matrix, 0, v2, 0);
+			Matrix.multiplyMV(r3, 0, matrix, 0, v3, 0);
+
+			float[] calculatedMatrix = getTransformationMatrix(v1, v2, v3, r1, r2, r3, maxDiff);
+
+			test(v1, r1, calculatedMatrix, maxDiff);
+			test(v2, r2, calculatedMatrix, maxDiff);
+			test(v3, r3, calculatedMatrix, maxDiff);
+
+			for (int j = 0; j < 10; j++) {
+				float[] q1 = new float[] { random.nextFloat(), random.nextFloat(), random.nextFloat(), 1 };
+				float[] q2 = new float[] { random.nextFloat(), random.nextFloat(), random.nextFloat(), 1 };
+				float[] q3 = new float[] { random.nextFloat(), random.nextFloat(), random.nextFloat(), 1 };
+				float[] b1 = new float[4];
+				float[] b2 = new float[4];
+				float[] b3 = new float[4];
+				Matrix.multiplyMV(b1, 0, matrix, 0, q1, 0);
+				Matrix.multiplyMV(b2, 0, matrix, 0, q2, 0);
+				Matrix.multiplyMV(b3, 0, matrix, 0, q3, 0);
+
+				test(q1, b1, calculatedMatrix, maxDiff);
+				test(q2, b2, calculatedMatrix, maxDiff);
+				test(q3, b3, calculatedMatrix, maxDiff);
+			}
+		}
+	}
+
+	private static void test1(float maxDiff) {
+		float[] v1 = new float[] { 1, 2, 0 };
+		float[] v2 = new float[] { 1, 1, 0 };
+		float[] v3 = new float[] { 3, 2, 0 };
+		float[] u1 = new float[] { 0, 2, 0 };
+		float[] u2 = new float[] { -1, 2, 0 };
+		float[] u3 = new float[] { 0, 2, 2 };
+
+		float[] transformationMatrix = getTransformationMatrix(v1, v2, v3, u1, u2, u3, maxDiff);
+
+		test(v1, u1, transformationMatrix, maxDiff);
+		test(v2, u2, transformationMatrix, maxDiff);
+		test(v3, u3, transformationMatrix, maxDiff);
+	}
+
+	private static void test2(float maxDiff) {
+		float[] v1 = new float[] { 3, 0, 0 };
+		float[] v2 = new float[] { 4, 0, 0 };
+		float[] v3 = new float[] { 4, 1, 0 };
+		float[] u1 = new float[] { 1, 3, 0 };
+		float[] u2 = new float[] { 0, 3, 0 };
+		float[] u3 = new float[] { 0, 2, 0 };
+
+		float[] transformationMatrix = getTransformationMatrix(v1, v2, v3, u1, u2, u3, maxDiff);
+
+		test(v1, u1, transformationMatrix, maxDiff);
+		test(v2, u2, transformationMatrix, maxDiff);
+		test(v3, u3, transformationMatrix, maxDiff);
+	}
+	
+	protected byte[] floatArrayToByteArray(float[] vertices) {
+		if (vertices == null) {
+			return null;
+		}
+		ByteBuffer buffer = ByteBuffer.wrap(new byte[vertices.length * 4]);
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		FloatBuffer asFloatBuffer = buffer.asFloatBuffer();
+		for (float f : vertices) {
+			asFloatBuffer.put(f);
+		}
+		return buffer.array();
+	}
+
+	protected byte[] intArrayToByteArray(int[] indices) {
+		if (indices == null) {
+			return null;
+		}
+		ByteBuffer buffer = ByteBuffer.wrap(new byte[indices.length * 4]);
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		IntBuffer asIntBuffer = buffer.asIntBuffer();
+		for (int i : indices) {
+			asIntBuffer.put(i);
+		}
+		return buffer.array();
+	}
+}
