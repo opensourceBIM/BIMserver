0c873dae0811477823e10034ce3c418b4be875b8
Ruben de Laat <ruben@logic-labs.nl>
2016-10-26 14:35:17 +0200

    Added getSubProjectByName method

---
+++ BimServer/src/org/bimserver/database/actions/GetSubProjectByNameDatabaseAction.java
@@ -0,0 +1,34 @@
+package org.bimserver.database.actions;
+
+import org.bimserver.BimserverDatabaseException;
+import org.bimserver.database.BimserverLockConflictException;
+import org.bimserver.database.DatabaseSession;
+import org.bimserver.models.log.AccessMethod;
+import org.bimserver.models.store.Project;
+import org.bimserver.shared.exceptions.UserException;
+import org.bimserver.webservices.authorization.Authorization;
+
+public class GetSubProjectByNameDatabaseAction extends BimDatabaseAction<Project> {
+
+	private final String name;
+	private Authorization authorization;
+	private Long parentProjectId;
+
+	public GetSubProjectByNameDatabaseAction(DatabaseSession databaseSession, AccessMethod accessMethod, Long parentProjectId, String name, Authorization authorization) {
+		super(databaseSession, accessMethod);
+		this.parentProjectId = parentProjectId;
+		this.name = name;
+		this.authorization = authorization;
+	}
+
+	@Override
+	public Project execute() throws UserException, BimserverLockConflictException, BimserverDatabaseException {
+		Project parent = getProjectByPoid(parentProjectId);
+		for (Project project : parent.getSubProjects()) {
+			if (project.getName().equals(name)) {
+				return project;
+			}
+		}
+		return null;
+	}
+}
