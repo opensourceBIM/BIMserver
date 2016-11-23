74a013ceb11d7c7c8fe46079441f0b17f012aea3
Ruben de Laat <ruben@logic-labs.nl>
2016-11-23 20:31:12 +0100

    Added a list of schemas as enum

---
+++ PluginBase/src/org/bimserver/plugins/SchemaName.java
@@ -0,0 +1,26 @@
+package org.bimserver.plugins;
+
+// https://github.com/opensourceBIM/BIMserver/wiki/New-remote-service-interface
+
+public enum SchemaName {
+	IFC_STEP_2X3TC1,
+	IFC_STEP_4,
+	IFC_XML_2x3TC1,
+	IFC_XML_4,
+	IFC_JSON_2x3TC1,
+	IFC_JSON_4,
+	IFC_JSON_GEOM_2x3TC1,
+	IFC_JSON_GEOM_4,
+	BCF_ZIP_1_0,
+	BCF_ZIP_2_0,
+	GLTF_1_0,
+	GLTF_BIN_1_0,
+	COLLADA_1_5,
+	KMZ_2_2_0,
+	BINARY_GEOMETRY_6,
+	CITYGML_2_0_0,
+	VIS_3D_JSON_1_0,
+	VALIDATION_JSON_1_0,
+	LOD_CSV_1_0,
+	INFO_3D_EXCEL_1_0;
+}
