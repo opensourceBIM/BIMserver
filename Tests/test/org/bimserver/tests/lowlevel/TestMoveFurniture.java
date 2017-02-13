08f8525f365d1726a5ae6f9470e14866f429de07
Ruben de Laat <ruben@logic-labs.nl>
2017-02-13 09:09:50 +0100

    Load testdata from github

--- Tests/test/org/bimserver/tests/lowlevel/TestMoveFurniture.java
+++ Tests/test/org/bimserver/tests/lowlevel/TestMoveFurniture.java
@@ -2,6 +2,7 @@ package org.bimserver.tests.lowlevel;
 
 import static org.junit.Assert.fail;
 
+import java.net.URL;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -32,7 +33,7 @@ public class TestMoveFurniture extends TestWithEmbeddedServer {
 			SDeserializerPluginConfiguration deserializer = bimServerClient.getServiceInterface().getSuggestedDeserializerForExtension("ifc", newProject.getOid());
 
 			// Checkin the file
-			bimServerClient.checkin(newProject.getOid(), "test", deserializer.getOid(), false, Flow.SYNC, Paths.get("../TestData/data/AC11-Institute-Var-2-IFC.ifc"));
+			bimServerClient.checkin(newProject.getOid(), "test", deserializer.getOid(), false, Flow.SYNC, new URL("https://github.com/opensourceBIM/TestFiles/raw/master/TestData/data/AC11-Institute-Var-2-IFC.ifc"));
 
 			// Refresh project info
 			newProject = bimServerClient.getServiceInterface().getProjectByPoid(newProject.getOid());
--- Tests/test/org/bimserver/tests/lowlevel/TestSetWrappedBoolean.java
+++ Tests/test/org/bimserver/tests/lowlevel/TestSetWrappedBoolean.java
@@ -2,6 +2,7 @@ package org.bimserver.tests.lowlevel;
 
 import static org.junit.Assert.fail;
 
+import java.net.URL;
 import java.nio.file.Paths;
 
 import org.bimserver.emf.IfcModelInterface;
@@ -28,7 +29,7 @@ public class TestSetWrappedBoolean extends TestWithEmbeddedServer {
 			SProject newProject = bimServerClient.getServiceInterface().addProject("test" + Math.random(), "ifc2x3tc1");
 			
 			SDeserializerPluginConfiguration suggestedDeserializerForExtension = bimServerClient.getServiceInterface().getSuggestedDeserializerForExtension("ifc", newProject.getOid());
-			bimServerClient.checkin(newProject.getOid(), "initial", suggestedDeserializerForExtension.getOid(), false, Flow.SYNC, Paths.get("../TestData/data/revit_quantities.ifc"));
+			bimServerClient.checkin(newProject.getOid(), "initial", suggestedDeserializerForExtension.getOid(), false, Flow.SYNC, new URL("https://github.com/opensourceBIM/TestFiles/raw/master/TestData/data/revit_quantities.ifc"));
 			newProject = bimServerClient.getServiceInterface().getProjectByPoid(newProject.getOid());
 			
 			SSerializerPluginConfiguration serializer = bimServerClient.getServiceInterface().getSerializerByName("Ifc2x3");
