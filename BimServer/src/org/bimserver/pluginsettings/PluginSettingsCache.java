4428ed139e56c41d822dfa8b1521ae4b01f12fe8
Ruben de Laat <ruben@logic-labs.nl>
2018-10-01 12:32:47 +0200

    Added PluginSettingsCache

---
+++ BimServer/src/org/bimserver/pluginsettings/PluginSettingsCache.java
@@ -0,0 +1,37 @@
+package org.bimserver.pluginsettings;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bimserver.BimServer;
+import org.bimserver.BimserverDatabaseException;
+import org.bimserver.database.DatabaseSession;
+import org.bimserver.database.OldQuery;
+import org.bimserver.plugins.PluginConfiguration;
+
+public class PluginSettingsCache {
+
+	private BimServer bimServer;
+	private final Map<Long, PluginConfiguration> pluginConfigurations = Collections.synchronizedMap(new HashMap<>());
+
+	public PluginSettingsCache(BimServer bimServer) {
+		this.bimServer = bimServer;
+	}
+	
+	public void reset(long serializerOid) {
+		this.pluginConfigurations.remove(serializerOid);
+	}
+
+	public PluginConfiguration getPluginSettings(Long serializerOid) throws BimserverDatabaseException {
+		PluginConfiguration pluginConfiguration = pluginConfigurations.get(serializerOid);
+		if (pluginConfiguration == null) {
+			try (DatabaseSession databaseSession = bimServer.getDatabase().createSession()) {
+				org.bimserver.models.store.PluginConfiguration serializerPluginConfiguration = databaseSession.get(serializerOid, OldQuery.getDefault());
+				pluginConfiguration = new org.bimserver.plugins.PluginConfiguration(serializerPluginConfiguration.getSettings());
+				pluginConfigurations.put(serializerOid, pluginConfiguration);
+			}
+		}
+		return pluginConfiguration;
+	}
+}
