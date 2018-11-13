a00bb88160387e0c1dc78fc11cc7427f18fc4893
Ruben de Laat <Ruben de Laat@DESKTOP-9900K>
2018-11-13 18:16:34 +0100

    Changed internal BimBotInterface to pass a PluginConfiguration instead of SObjectType settings. This is more usable and requires no conversion but more importantly no open database session.

--- PluginBase/src/org/bimserver/bimbots/BimBotsServiceInterface.java
+++ PluginBase/src/org/bimserver/bimbots/BimBotsServiceInterface.java
@@ -21,10 +21,10 @@ import java.util.Set;
 
 import org.bimserver.database.queries.om.Query;
 import org.bimserver.emf.PackageMetaData;
-import org.bimserver.interfaces.objects.SObjectType;
+import org.bimserver.plugins.PluginConfiguration;
 
 public interface BimBotsServiceInterface {
-	BimBotsOutput runBimBot(BimBotsInput input, BimBotContext bimBotContext, SObjectType settings) throws BimBotsException;
+	BimBotsOutput runBimBot(BimBotsInput input, BimBotContext bimBotContext, PluginConfiguration settings) throws BimBotsException;
 	boolean requiresGeometry();
 	Set<String> getAvailableInputs() throws BimBotsException;
 	Set<String> getAvailableOutputs() throws BimBotsException;
