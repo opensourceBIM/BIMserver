f3885063f9d6aff86039da580b50972920087d78
Ruben de Laat <Ruben de Laat@DESKTOP-9900K>
2018-11-13 18:18:20 +0100

    Initialize validationMetaData

--- Shared/src/org/bimserver/validationreport/RootIssueContainer.java
+++ Shared/src/org/bimserver/validationreport/RootIssueContainer.java
@@ -4,6 +4,10 @@ public class RootIssueContainer extends IssueContainer {
 
 	private ValidationMetaData validationMetaData;
 
+	public RootIssueContainer() {
+		this.validationMetaData = new ValidationMetaData();
+	}
+	
 	public void setValidationMetaData(ValidationMetaData validationMetaData) {
 		this.validationMetaData = validationMetaData;
 	}
