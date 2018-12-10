55afd7fd9991536ff6efd9c108cb5ae855e98e33
Ruben de Laat <ruben@logic-labs.nl>
2018-12-10 16:45:57 +0100

    Fixed referencing errors

--- BimServer/src/org/bimserver/geometry/ColorMap2.java
+++ BimServer/src/org/bimserver/geometry/ColorMap2.java
@@ -3,30 +3,41 @@ package org.bimserver.geometry;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class ColorMap2 {
 	private static class A {
 
-		private byte[] color;
+		private byte[] color = new byte[4];
 		public int count = 4;
 
 		public A(byte[] color) {
-			this.color = color;
+			System.arraycopy(color, 0, this.color, 0, color.length);
 		}
 	}
 	
-	private byte[] lastColor;
+	private byte[] lastColor = new byte[4];
 	private final List<A> list = new ArrayList<>();
 	
 	private A lastA;
 	
+	private boolean arrayEquals(byte[] a, byte[] a2) {
+        int length = a.length;
+        if (a2.length != length)
+            return false;
+
+        for (int i=0; i<length; i++)
+            if (a[i] != a2[i])
+                return false;
+        
+        return true;
+	}
+	
 	public void addColor(byte[] color) {
-		if (lastColor == null || !Arrays.equals(lastColor, color)) {
+		if (lastColor == null || !arrayEquals(lastColor, color)) {
 			lastA = new A(color);
 			list.add(lastA);
-			lastColor = color;
+			System.arraycopy(color, 0, lastColor, 0, color.length);
 		} else {
 			lastA.count += 4;
 		}
@@ -41,5 +52,9 @@ public class ColorMap2 {
 			buffer.put(color);
 		}
 		return buffer.array();
+	}
+
+	public int size() {
+		return list.size();
 	}
 }
