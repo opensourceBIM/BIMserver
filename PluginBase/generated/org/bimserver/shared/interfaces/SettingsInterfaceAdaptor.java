d2a0e520f00d4724480b914f1626d6cb8d7c5eee
Ruben de Laat <ruben@logic-labs.nl>
2019-01-29 14:09:32 +0100

    Generated

--- PluginBase/generated/org/bimserver/shared/interfaces/SettingsInterfaceAdaptor.java
+++ PluginBase/generated/org/bimserver/shared/interfaces/SettingsInterfaceAdaptor.java
@@ -16,7 +16,6 @@ package org.bimserver.shared.interfaces;
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see {@literal<http://www.gnu.org/licenses/>}.
  *****************************************************************************/
-
 import org.bimserver.shared.exceptions.UserException;
 import org.bimserver.shared.exceptions.ServerException;
 
