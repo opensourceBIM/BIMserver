6bda9c78a6a4264201b499868a60da0fc583579c
Ruben de Laat <ruben@logic-labs.nl>
2019-07-12 14:13:59 +0200

    Added simple client test

---
+++ BimServerClientLib/test/org/bimserver/client/tests/TestCheckinFromUrlAsync.java
@@ -0,0 +1,40 @@
+package org.bimserver.client.tests;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.bimserver.client.BimServerClient;
+import org.bimserver.client.json.JsonBimServerClientFactory;
+import org.bimserver.interfaces.objects.SActionState;
+import org.bimserver.interfaces.objects.SDeserializerPluginConfiguration;
+import org.bimserver.interfaces.objects.SLongActionState;
+import org.bimserver.interfaces.objects.SProject;
+import org.bimserver.interfaces.objects.SService;
+import org.bimserver.shared.UsernamePasswordAuthenticationInfo;
+import org.bimserver.shared.exceptions.BimServerClientException;
+import org.junit.Test;
+
+public class TestCheckinFromUrlAsync {
+	@Test
+	public void test() {
+		try (JsonBimServerClientFactory factory = new JsonBimServerClientFactory("http://localhost:8080")) {
+			try (BimServerClient client = factory.create(new UsernamePasswordAuthenticationInfo("admin@bimserver.org", "admin"))) {
+				SProject project = client.getServiceInterface().addProject(RandomStringUtils.random(10), "ifc2x3tc1");
+				
+				SDeserializerPluginConfiguration deserializer = client.getServiceInterface().getSuggestedDeserializerForExtension("ifc", project.getOid());
+				String url = "https://github.com/opensourceBIM/TestFiles/raw/master/TestData/data/AC11-Institute-Var-2-IFC.ifc";
+				Long topicId = client.getServiceInterface().checkinFromUrlAsync(project.getOid(), "test", deserializer.getOid(), "test", url, false);
+				for (int i=0; i<100; i++) {
+					SLongActionState progress = client.getNotificationRegistryInterface().getProgress(topicId);
+					System.out.println(progress.getState());
+					if (progress.getState() == SActionState.FINISHED) {
+						break;
+					}
+					Thread.sleep(1000);
+				}
+			}
+		} catch (BimServerClientException e) {
+			e.printStackTrace();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+	}
+}
