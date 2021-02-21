a1a6a46abf1aaf46fd5ac79414df666dd44e8cd4
hlg <public.void@gmx.de>
2021-02-21 19:12:14 +0100

    another test made somewhat useful

--- BimServerClientLib/test/org/bimserver/client/tests/TestListActivity.java
+++ BimServerClientLib/test/org/bimserver/client/tests/TestListActivity.java
@@ -17,12 +17,10 @@ import org.junit.Test;
 public class TestListActivity {
 	@Test
 	public void test() throws Exception {
-		try (JsonBimServerClientFactory factory = new JsonBimServerClientFactory("[address]")) {
-			try (BimServerClient client = factory.create(new UsernamePasswordAuthenticationInfo("[un]", "[pw]"))) {
+		try (JsonBimServerClientFactory factory = new JsonBimServerClientFactory("http://localhost:8080")) {
+			try (BimServerClient client = factory.create(new UsernamePasswordAuthenticationInfo("admin@bimserver.org", "admin"))) {
 				SServerInfo serverInfo = client.getAdminInterface().getServerInfo();
-
 				Map<Date, SRevision> revisions = new TreeMap<>();
-				
 				for (SProject sProject : client.getServiceInterface().getAllProjects(true, true)) {
 					for (Long roid : sProject.getRevisions()) {
 						SRevision revision = client.getServiceInterface().getRevision(roid);
@@ -30,7 +28,6 @@ public class TestListActivity {
 						revisions.put(date, revision);
 					}
 				}
-				
 				DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd hh:mm");
 				for (SRevision revision : revisions.values()) {
 					System.out.println(dateFormatter.format(revision.getDate()) + " " + revision.getComment());
