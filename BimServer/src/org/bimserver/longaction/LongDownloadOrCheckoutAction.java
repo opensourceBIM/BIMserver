opensourceBIM/BIMserver

commit e808e03732a9cac114ff3f5eaee81d1ccb7082a2

cleanup JsonBimServerClientFactory creation with custom SSL certificate

BimServerClientLib/src/org/bimserver/client/AbstractBimServerClientFactory.java
@@ -18,11 +18,27 @@ package org.bimserver.client;
  *****************************************************************************/
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
 import org.bimserver.emf.MetaDataManager;
 import org.bimserver.interfaces.SServiceInterfaceService;
 import org.bimserver.plugins.services.BimServerClientInterface;
@@ -45,6 +61,8 @@ import org.bimserver.shared.interfaces.SettingsInterface;
 import org.bimserver.shared.meta.SService;
 import org.bimserver.shared.meta.SServicesMap;
 
+import javax.net.ssl.SSLContext;
+
 public abstract class AbstractBimServerClientFactory implements BimServerClientFactory {
 
 	private final SServicesMap servicesMap;
@@ -57,10 +75,10 @@ public abstract class AbstractBimServerClientFactory implements BimServerClientF
 			throw new IllegalArgumentException("MetaDataManager cannot be null");
 		}
 		this.metaDataManager = metaDataManager;
-		initHttpClient();
+		initHttpClient(null);
 	}
 
-	public AbstractBimServerClientFactory(MetaDataManager metaDataManager) throws BimServerClientException {
+	public AbstractBimServerClientFactory(MetaDataManager metaDataManager, URL trustedCertificate) throws BimServerClientException {
 		if (metaDataManager == null) {
 			try {
 				this.metaDataManager = new MetaDataManager(Files.createTempDirectory("bimserver-tmp"));
@@ -87,22 +105,35 @@ public abstract class AbstractBimServerClientFactory implements BimServerClientF
 		addService(new SService(servicesMap, null, NotificationRegistryInterface.class));
 		addService(new SService(servicesMap, null, OAuthInterface.class));
 		servicesMap.initialize();
-		initHttpClient();
+		initHttpClient(sslContext(trustedCertificate));
+	}
+
+	public AbstractBimServerClientFactory(MetaDataManager metaDataManager) throws BimServerClientException {
+		this(metaDataManager, null);
 	}
 	
 	public CloseableHttpClient getHttpClient() {
 		return httpClient;
 	}
 	
-	public void initHttpClient() {
+	private void initHttpClient(SSLContext sslContext) {
 		HttpClientBuilder builder = HttpClientBuilder.create();
-		
-		PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager();
+		SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+			sslContext == null ? SSLContexts.createSystemDefault() :  sslContext,
+			new String[] { "TLSv1.2" },
+			null,
+			SSLConnectionSocketFactory.getDefaultHostnameVerifier()
+		);
+		Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+				.register("http", PlainConnectionSocketFactory.getSocketFactory())
+				.register("https", sslsf)
+				.build();
+		PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(registry);
 		connManager.setMaxTotal(100);
 		connManager.setDefaultMaxPerRoute(100);
 		builder.setConnectionManager(connManager);
 		builder.disableAutomaticRetries();
-		
+		// TODO set timeouts? https://hc.apache.org/httpcomponents-client-5.1.x/migration-guide/preparation.html
 //		builder.addInterceptorFirst(new HttpRequestInterceptor() {
 //			public void process(final HttpRequest request, final HttpContext context) throws HttpException, IOException {
 //				if (!request.containsHeader("Accept-Encoding")) {
@@ -131,7 +162,29 @@ public abstract class AbstractBimServerClientFactory implements BimServerClientF
 
 		httpClient = builder.build();
 	}
-	
+
+	private SSLContext sslContext(URL trustedCertificate) throws BimServerClientException {
+		if(trustedCertificate != null) {
+			SSLContextBuilder sslContextBuilder = SSLContexts.custom();
+			try {
+				KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+				keystore.load(null);  // initializes keystore
+				CertificateFactory cf = CertificateFactory.getInstance("X.509");
+				Certificate cert = null;
+				try (InputStream trustedCertStream = trustedCertificate.openStream()) {
+					cert = cf.generateCertificate(trustedCertStream);
+				}
+				if (cert!=null) keystore.setCertificateEntry("onlyentry", cert);
+				sslContextBuilder.loadTrustMaterial(keystore, null);
+				return sslContextBuilder.build();
+			} catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException | KeyManagementException  e) {
+				throw new BimServerClientException("Unable to use provided certificate.");
+			}
+		} else {
+			return SSLContexts.createSystemDefault();
+		}
+	}
+
 	public MetaDataManager getMetaDataManager() {
 		return metaDataManager;
 	}

BimServerClientLib/src/org/bimserver/client/json/JsonBimServerClientFactory.java
@@ -27,6 +27,7 @@ import org.bimserver.shared.exceptions.ServiceException;
 import org.bimserver.shared.meta.SServicesMap;
 import org.bimserver.shared.reflector.RealtimeReflectorFactoryBuilder;
 import org.bimserver.shared.reflector.ReflectorFactory;
+import java.net.URL;
 
 public class JsonBimServerClientFactory extends AbstractBimServerClientFactory {
 
@@ -48,7 +49,8 @@ public class JsonBimServerClientFactory extends AbstractBimServerClientFactory {
 	 * @param address Address of the remote server, must have the format "protocol://address:port[/contextpath]", for example "http://localhost:8080"
 	 * @throws BimServerClientException 
 	 */
-	public JsonBimServerClientFactory(MetaDataManager metaDataManager, String address) throws BimServerClientException {
+
+	public JsonBimServerClientFactory(MetaDataManager metaDataManager, String address, URL trustedCertificate) throws BimServerClientException {
 		super(metaDataManager);
 		this.address = address;
 		this.jsonSocketReflectorFactory = new JsonSocketReflectorFactory(getServicesMap(), getHttpClient());
@@ -59,6 +61,14 @@ public class JsonBimServerClientFactory extends AbstractBimServerClientFactory {
 
 	public JsonBimServerClientFactory(String address) throws BimServerClientException {
 		this(null, address);
+	}
+
+	public JsonBimServerClientFactory(String address, URL trustedCertificate) throws BimServerClientException {
+		this(null, address, trustedCertificate);
+	}
+
+	public JsonBimServerClientFactory(MetaDataManager metaDataManager, String address) throws BimServerClientException {
+		this(metaDataManager, address, null);
 	}
 
 	@Override

BimServerClientLib/src/org/bimserver/client/json/JsonBimServerSSLClientFactory.java
@@ -1,109 +1,0 @@
-package org.bimserver.client.json;
-
-/******************************************************************************
- * Copyright (C) 2009-2019  BIMserver.org
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see {@literal<http://www.gnu.org/licenses/>}.
- *****************************************************************************/
-
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.HttpClientConnectionManager;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.ssl.SSLContexts;
-import org.bimserver.client.BimServerClient;
-import org.bimserver.shared.AuthenticationInfo;
-import org.bimserver.shared.ChannelConnectionException;
-import org.bimserver.shared.exceptions.BimServerClientException;
-import org.bimserver.shared.exceptions.ServiceException;
-import org.bimserver.shared.reflector.RealtimeReflectorFactoryBuilder;
-import org.bimserver.shared.reflector.ReflectorFactory;
-
-import javax.net.ssl.SSLContext;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.security.KeyManagementException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-
-public class JsonBimServerSSLClientFactory extends JsonBimServerClientFactory {
-
-  private final JsonSSLSocketReflectorFactory jsonReflectorFactory;
-  private final ReflectorFactory reflectorFactory;
-  private final String address;
-  private final SSLConnectionSocketFactory sslsf;
-  private CloseableHttpClient sslHttpClient;
-
-  public JsonBimServerSSLClientFactory(String address, URL trustedCertificate) throws BimServerClientException, CertificateException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException {
-    super(null, address); // actually we only want to call AbstractBimServerClientFactory(null) to get the service map instantiated
-    this.sslsf = new SSLConnectionSocketFactory(sslContext(trustedCertificate));
-    this.jsonReflectorFactory = new JsonSSLSocketReflectorFactory(getServicesMap(), newConnectionManager());
-    this.reflectorFactory = new RealtimeReflectorFactoryBuilder(getServicesMap()).newReflectorFactory();
-    this.address = address;
-    sslHttpClient = HttpClients.custom().setConnectionManager(newConnectionManager()).build();
-  }
-
-  public CloseableHttpClient getHttpClient() {
-    return sslHttpClient;
-  }
-
-  @Override
-  public BimServerClient create(AuthenticationInfo authenticationInfo) throws ServiceException, ChannelConnectionException {
-    JsonChannel jsonChannel = new JsonChannel(getHttpClient(), reflectorFactory, jsonReflectorFactory, address + "/json", getServicesMap());
-    BimServerClient bimServerClient = new BimServerClient(this.getMetaDataManager(), address, getServicesMap(), jsonChannel);
-    jsonChannel.connect(bimServerClient);
-    bimServerClient.setAuthentication(authenticationInfo);
-    bimServerClient.connect();
-    return bimServerClient;
-
-  }
-
-  private HttpClientConnectionManager newConnectionManager() {
-    Registry<ConnectionSocketFactory> r = RegistryBuilder.<ConnectionSocketFactory>create()
-      .register("http", PlainConnectionSocketFactory.getSocketFactory())
-      .register("https", sslsf)
-      .build();
-    PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(r);
-    connManager.setMaxTotal(100);
-    connManager.setDefaultMaxPerRoute(100);
-    return connManager;
-  }
-
-  private SSLContext sslContext(URL trustedCertificate) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, KeyManagementException {
-    SSLContextBuilder sslContextBuilder = SSLContexts.custom();
-    if(trustedCertificate != null) {
-      KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-      keystore.load(null);  // initializes keystore
-      CertificateFactory cf = CertificateFactory.getInstance("X.509");
-      Certificate cert = null;
-      try (InputStream trustedCertStream = trustedCertificate.openStream()) {
-        cert = cf.generateCertificate(trustedCertStream);
-      }
-      if (cert!=null) keystore.setCertificateEntry("onlyentry", cert);
-      sslContextBuilder.loadTrustMaterial(keystore, null);
-    }
-    return sslContextBuilder.build();
-  }
-}

BimServerClientLib/src/org/bimserver/client/json/JsonSSLSocketReflectorFactory.java
@@ -1,47 +1,0 @@
-package org.bimserver.client.json;
-
-/******************************************************************************
- * Copyright (C) 2009-2019  BIMserver.org
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see {@literal<http://www.gnu.org/licenses/>}.
- *****************************************************************************/
-
-import org.apache.http.conn.HttpClientConnectionManager;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.bimserver.shared.TokenHolder;
-import org.bimserver.shared.json.JsonSocketReflector;
-import org.bimserver.shared.meta.SServicesMap;
-
-public class JsonSSLSocketReflectorFactory extends JsonSocketReflectorFactory {
-
-    public JsonSSLSocketReflectorFactory(SServicesMap servicesMap, HttpClientConnectionManager connectionManager) {
-        super(servicesMap);
-        this.servicesMap = servicesMap;
-        sslHttpClientConnectionManager = connectionManager;
-        sslHttpClient = HttpClients.custom().setConnectionManager(connectionManager).build();
-    }
-
-        private SServicesMap servicesMap;
-        private CloseableHttpClient sslHttpClient;
-        private HttpClientConnectionManager sslHttpClientConnectionManager;
-
-        public void close() {
-            sslHttpClientConnectionManager.shutdown();
-        }
-
-        public JsonSocketReflector create(String remoteAddress, TokenHolder tokenHolder) {
-            return new JsonSocketReflector(sslHttpClient, servicesMap, remoteAddress, tokenHolder);
-        }
-}

commit 6daa43afdda3d25ebbd41e0d0de89361063e7142

Java client HTTPS handling: use SSL certificate if provided
This fixes a regression introduced in e808e03.

BimServer/src/org/bimserver/longaction/LongDownloadOrCheckoutAction.java
@@ -77,7 +77,7 @@ public abstract class LongDownloadOrCheckoutAction extends LongAction<DownloadPa
 			try {
 				Serializer serializer = getBimServer().getSerializerFactory().create(project, username, model, renderEnginePlugin, downloadParameters);
 				if (serializer == null) {
-					throw new UserException("Error, no serializer found " + downloadParameters.getSerializerOid());
+					throw new UserException("Error, serializer " + downloadParameters.getSerializerOid() + "not found or failed to intialize.");
 				}
 				if (getBimServer().getServerSettingsCache().getServerSettings().getCacheOutputFiles() && serializer.allowCaching()) {
 					if (getBimServer().getDiskCacheManager().contains(downloadParameters)) {

BimServerClientLib/src/org/bimserver/client/AbstractBimServerClientFactory.java
@@ -105,7 +105,7 @@ public abstract class AbstractBimServerClientFactory implements BimServerClientF
 		addService(new SService(servicesMap, null, NotificationRegistryInterface.class));
 		addService(new SService(servicesMap, null, OAuthInterface.class));
 		servicesMap.initialize();
-		initHttpClient(sslContext(trustedCertificate));
+		initHttpClient(trustedCertificate != null  ? sslContext(trustedCertificate) : null);
 	}
 
 	public AbstractBimServerClientFactory(MetaDataManager metaDataManager) throws BimServerClientException {

BimServerClientLib/src/org/bimserver/client/json/JsonBimServerClientFactory.java
@@ -51,7 +51,7 @@ public class JsonBimServerClientFactory extends AbstractBimServerClientFactory {
 	 */
 
 	public JsonBimServerClientFactory(MetaDataManager metaDataManager, String address, URL trustedCertificate) throws BimServerClientException {
-		super(metaDataManager);
+		super(metaDataManager, trustedCertificate);
 		this.address = address;
 		this.jsonSocketReflectorFactory = new JsonSocketReflectorFactory(getServicesMap(), getHttpClient());
 		RealtimeReflectorFactoryBuilder factoryBuilder = new RealtimeReflectorFactoryBuilder(getServicesMap());