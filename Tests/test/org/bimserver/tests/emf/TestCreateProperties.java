ac491dcd3dc55740643f1c102bebc3378627ba1b
hlg <public.void@gmx.de>
2025-02-14 20:17:22 +0100

    simplify test, remove redundant try/catch/fail
    This is an example, should be done for all tests that expose this
    anti-pattern.

--- Tests/test/org/bimserver/tests/emf/TestCreateProperties.java
+++ Tests/test/org/bimserver/tests/emf/TestCreateProperties.java
@@ -17,8 +17,9 @@ package org.bimserver.tests.emf;
  * along with this program.  If not, see {@literal<http://www.gnu.org/licenses/>}.
  *****************************************************************************/
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
@@ -39,7 +40,10 @@ import org.bimserver.models.ifc2x3tc1.IfcWindow;
 import org.bimserver.models.ifc2x3tc1.Tristate;
 import org.bimserver.plugins.services.BimServerClientInterface;
 import org.bimserver.plugins.services.Flow;
+import org.bimserver.shared.ChannelConnectionException;
 import org.bimserver.shared.UsernamePasswordAuthenticationInfo;
+import org.bimserver.shared.exceptions.BimServerClientException;
+import org.bimserver.shared.exceptions.ServiceException;
 import org.bimserver.test.TestWithEmbeddedServer;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -48,59 +52,57 @@ import org.slf4j.LoggerFactory;
 public class TestCreateProperties extends TestWithEmbeddedServer  {
 	private static final Logger LOGGER = LoggerFactory.getLogger(TestCreateProperties.class);
 	@Test
-	public void test() {
-		try {
-			// New client
-			BimServerClientInterface bimServerClient = getFactory().create(new UsernamePasswordAuthenticationInfo("admin@bimserver.org", "admin"));
-			
-			// Create a project
-			SProject project = bimServerClient.getServiceInterface().addProject("test" + Math.random(), "ifc2x3tc1");
-			
-			// Look for a deserializer
-			SDeserializerPluginConfiguration deserializer = bimServerClient.getServiceInterface().getSuggestedDeserializerForExtension("ifc", project.getOid());
-			
-			bimServerClient.checkinSync(project.getOid(), "test", deserializer.getOid(), false, new URL("https://github.com/opensourceBIM/TestFiles/raw/master/TestData/data/AC11-Institute-Var-2-IFC.ifc"));
-			
-			// Refresh project
-			project = bimServerClient.getServiceInterface().getProjectByPoid(project.getOid());
-			
-			// Load model without lazy loading (complete model at once)
-			IfcModelInterface model = bimServerClient.getModel(project, project.getLastRevisionId(), true, true);
+	public void test() throws ServiceException, ChannelConnectionException, MalformedURLException, BimServerClientException, IfcModelInterfaceException {
+		// New client
+		BimServerClientInterface bimServerClient = getFactory().create(new UsernamePasswordAuthenticationInfo("admin@bimserver.org", "admin"));
 
-			String propertyName = "BooleanProperty";
+		// Create a project
+		SProject project = bimServerClient.getServiceInterface().addProject("test" + Math.random(), "ifc2x3tc1");
 
-			int nrWindowsFirst = 0;
-			// Iterate over all projects, there should be 1
-			for (IfcWindow window : model.getAllWithSubTypes(IfcWindow.class)) {
-				nrWindowsFirst++;
-				createProperty(window, model, propertyName, "Description of property", true);
-			}
-			
-			long newRoid = model.commit("Added boolean properties to " + nrWindowsFirst + " windows");
-			
-			model = bimServerClient.getModel(project, newRoid, true, false);
-			int foundOke = 0;
-			int nrWindowsSecond = 0;
-			Set<Long> counted = new HashSet<Long>();
-			for (IfcWindow window : model.getAllWithSubTypes(IfcWindow.class)) {
-				nrWindowsSecond++;
-				for (IfcRelDefines ifcRelDefines : window.getIsDefinedBy()) {
-					if (ifcRelDefines instanceof IfcRelDefinesByProperties) {
-						IfcRelDefinesByProperties ifcRelDefinesByProperties  = (IfcRelDefinesByProperties)ifcRelDefines;
-						IfcPropertySetDefinition relatingPropertyDefinition = ifcRelDefinesByProperties.getRelatingPropertyDefinition();
-						if (relatingPropertyDefinition instanceof IfcPropertySet) {
-							IfcPropertySet ifcPropertySet = (IfcPropertySet)relatingPropertyDefinition;
-							for (IfcProperty ifcProperty : ifcPropertySet.getHasProperties()) {
-								if (ifcProperty instanceof IfcPropertySingleValue) {
-									IfcPropertySingleValue ifcPropertySingleValue = (IfcPropertySingleValue)ifcProperty;
-									if (ifcPropertySingleValue.getName().equals(propertyName)) {
-										IfcValue nominalValue = ifcPropertySingleValue.getNominalValue();
-										if (nominalValue instanceof IfcBoolean) {
-											if (((IfcBoolean)nominalValue).getWrappedValue() == Tristate.TRUE) {
-												if (!counted.contains(ifcPropertySingleValue.getOid())) {
-													foundOke++;
-													counted.add(ifcPropertySingleValue.getOid());
-												}
+		// Look for a deserializer
+		SDeserializerPluginConfiguration deserializer = bimServerClient.getServiceInterface().getSuggestedDeserializerForExtension("ifc", project.getOid());
+
+		bimServerClient.checkinSync(project.getOid(), "test", deserializer.getOid(), false, new URL("https://github.com/opensourceBIM/TestFiles/raw/master/TestData/data/AC11-Institute-Var-2-IFC.ifc"));
+
+		// Refresh project
+		project = bimServerClient.getServiceInterface().getProjectByPoid(project.getOid());
+
+		// Load model without lazy loading (complete model at once)
+		IfcModelInterface model = bimServerClient.getModel(project, project.getLastRevisionId(), true, true);
+
+		String propertyName = "BooleanProperty";
+
+		int nrWindowsFirst = 0;
+		// Iterate over all projects, there should be 1
+		for (IfcWindow window : model.getAllWithSubTypes(IfcWindow.class)) {
+			nrWindowsFirst++;
+			createProperty(window, model, propertyName, "Description of property", true);
+		}
+
+		long newRoid = model.commit("Added boolean properties to " + nrWindowsFirst + " windows");
+
+		model = bimServerClient.getModel(project, newRoid, true, false);
+		int foundOke = 0;
+		int nrWindowsSecond = 0;
+		Set<Long> counted = new HashSet<Long>();
+		for (IfcWindow window : model.getAllWithSubTypes(IfcWindow.class)) {
+			nrWindowsSecond++;
+			for (IfcRelDefines ifcRelDefines : window.getIsDefinedBy()) {
+				if (ifcRelDefines instanceof IfcRelDefinesByProperties) {
+					IfcRelDefinesByProperties ifcRelDefinesByProperties  = (IfcRelDefinesByProperties)ifcRelDefines;
+					IfcPropertySetDefinition relatingPropertyDefinition = ifcRelDefinesByProperties.getRelatingPropertyDefinition();
+					if (relatingPropertyDefinition instanceof IfcPropertySet) {
+						IfcPropertySet ifcPropertySet = (IfcPropertySet)relatingPropertyDefinition;
+						for (IfcProperty ifcProperty : ifcPropertySet.getHasProperties()) {
+							if (ifcProperty instanceof IfcPropertySingleValue) {
+								IfcPropertySingleValue ifcPropertySingleValue = (IfcPropertySingleValue)ifcProperty;
+								if (ifcPropertySingleValue.getName().equals(propertyName)) {
+									IfcValue nominalValue = ifcPropertySingleValue.getNominalValue();
+									if (nominalValue instanceof IfcBoolean) {
+										if (((IfcBoolean)nominalValue).getWrappedValue() == Tristate.TRUE) {
+											if (!counted.contains(ifcPropertySingleValue.getOid())) {
+												foundOke++;
+												counted.add(ifcPropertySingleValue.getOid());
 											}
 										}
 									}
@@ -110,16 +112,11 @@ public class TestCreateProperties extends TestWithEmbeddedServer  {
 					}
 				}
 			}
-			LOGGER.info("Windows first: " + nrWindowsFirst);
-			LOGGER.info("Windows second: " + nrWindowsSecond);
-			LOGGER.info("Found Oke: " + foundOke);
-			if (foundOke != nrWindowsFirst) {
-				fail(foundOke + " / " + nrWindowsFirst);
-			}
-		} catch (Throwable e) {
-			e.printStackTrace();
-			fail(e.getMessage());
 		}
+		LOGGER.info("Windows first: " + nrWindowsFirst);
+		LOGGER.info("Windows second: " + nrWindowsSecond);
+		LOGGER.info("Found Oke: " + foundOke);
+		assertEquals(foundOke, nrWindowsFirst);
 	}
 
 	private void createProperty(IfcWindow window, IfcModelInterface model, String name, String description, Object value) throws IfcModelInterfaceException {
